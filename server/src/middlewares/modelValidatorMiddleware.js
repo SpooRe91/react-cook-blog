c69461f6f859f5e76d5d8d8cd96e04bd0382519b
Martin B <m.bogdanov9110@gmail.com>
2022-07-04 17:12:06 +0300

    Create modelValidatorMiddleware.js

---
+++ server/src/middlewares/modelValidatorMiddleware.js
@@ -0,0 +1,11 @@
+//function that returns a middleware, uses the Mongoose validator on a action level
+const { getErrorMessage } = require("../utils/errorHelpers");
+
+exports.modelValidator = (Model) => async (req, res, next) => {
+        try {
+                await Model.validate(req.body);
+                next();
+        } catch (error) {
+                res.status(401).json({ error: getErrorMessage(error) });
+        };
+}
