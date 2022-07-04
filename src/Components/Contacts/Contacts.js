a8c2701c95b6fff03a483e25eabc6a796a6a544e
Martin B <m.bogdanov9110@gmail.com>
2022-07-04 17:12:30 +0300

    Create Contacts.js

---
+++ src/Components/Contacts/Contacts.js
@@ -0,0 +1,12 @@
+export const Contacts = () => {
+    return (
+        <div className="already-reg">
+            <h1 className="already-reg">Cook-blog by Martin Bogdanov - contact</h1>
+            <p className="about-text">
+                "Cook-blog" is a basic website created with "express.js" and "handlebars" entirely on back-end ran on
+                Node.js. Please enjoy the webiste and don't hesitate to <strong>contact</strong> me for further
+                suggestions at <span><strong>m.bogdanov9110@gmail.com</strong></span>, or at:
+                <a href="/about">Martin Bogdanov</a>
+            </p>
+        </div>);
+}
