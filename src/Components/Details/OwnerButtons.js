5eedd05fc8a956d94df5cc386f95ae0d2d5d28e2
Martin B <m.bogdanov9110@gmail.com>
2022-07-05 15:52:12 +0300

    remove

--- src/Components/Details/OwnerButtons.js
+++
@@ -1,9 +0,0 @@
-export const OnwerButtons = (props) => {
-    return (
-        <>
-            <a className="btn" href={`/edit/${props.meal._id}`}>Промени</a>
-            <a className="btn" href={`/delete/${props.meal._id}`}>Изтрии</a>
-            <a className="btn" href="/recipe/browse">Назад</a>
-        </>
-    );
-}
