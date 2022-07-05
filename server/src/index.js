046e64db5b2ffe3ce05c4881727aa8fd046c2fec
Martin B <m.bogdanov9110@gmail.com>
2022-07-05 15:54:09 +0300

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -3,15 +3,14 @@ const { PORT } = require('./config/env');
 const dbService = require('./config/mongoseConfig');
 const { auth } = require('./middlewares/authMiddleware');
 const { errorHandler } = require('./middlewares/errorHandlerMiddleware');
-
+const cors = require('./middlewares/corsMiddleware');
 const app = express();
 
 dbService.connecter();
 require('./config/cookieParserConfig')(app);//cookie parser
-require('./config/sessionConfig')(app);//express-session
-require('./config/handlebarsConfig')(app);//handlebars config
+app.use(cors());
 app.use(auth);//auth middleware
 require('./config/expressConfig')(app);//express config
 app.use(errorHandler)//error handler
 
-app.listen(PORT, console.log(`Listening on port http://localhost:${PORT}`));
+app.listen(PORT, console.log(`"Cook-blog API" listening to port http://localhost:${PORT}`), console.log(`"Cook-blog API" documentation is available at http://localhost:${PORT}/readme`));
