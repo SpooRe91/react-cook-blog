b2bf998c7a1752e222afac5aac3ac840aec79d69
Martin B <m.bogdanov9110@gmail.com>
2022-07-05 15:54:04 +0300

    Update createController.js

--- server/src/controllers/createController.js
+++ server/src/controllers/createController.js
@@ -6,16 +6,17 @@ const { modelValidator } = require('../middlewares/modelValidatorMiddleware');
 const { Meal } = require('../models/Meal');
 const { getErrorMessage } = require('../utils/errorHelpers');
 
-router.post('/add', modelValidator(Meal), async (req, res) => {
+router.post('/add', modelValidator(Meal), isAuth, async (req, res) => {
 
     const recipe = { ...req.body, owner: req.user._id };
 
     try {
         const created = await createRecipe.create(recipe);
-        res.status(201).json(created);
+        res.json(created);
 
     } catch (error) {
-        res.status(404).json({ error: getErrorMessage(error.message) });
+        console.error(error);
+        res.status(400).json({ error: getErrorMessage(error) });
     }
 });
 module.exports = router;
