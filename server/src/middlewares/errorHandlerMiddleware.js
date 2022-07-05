b89b0942f3da58bea78b4aaf683ee1446f3ee71b
Martin B <m.bogdanov9110@gmail.com>
2022-07-05 15:54:13 +0300

    Update errorHandlerMiddleware.js

--- server/src/middlewares/errorHandlerMiddleware.js
+++ server/src/middlewares/errorHandlerMiddleware.js
@@ -2,5 +2,5 @@ const { getErrorMessage } = require("../utils/errorHelpers");
 
 exports.errorHandler = (err, req, res, next) => {
     const status = err.status || 404;
-    res.status(status).render("404", { error: getErrorMessage(err) });
+    res.status(404).json({ error: getErrorMessage(err.message) });
 };
