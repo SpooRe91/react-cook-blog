36fcbe832f7eabd97d847a6fe11f0152a317cb21
Martin B <m.bogdanov9110@gmail.com>
2022-07-05 15:54:01 +0300

    Update mongoseConfig.js

--- server/src/config/mongoseConfig.js
+++ server/src/config/mongoseConfig.js
@@ -2,9 +2,14 @@ const mongoose = require('mongoose');
 const { DB_QUERY_STRING } = require('./env');
 
 
-exports.connecter = () => {
-    mongoose.connection.on('open', () => console.log('DB connected'));
-    return mongoose.connect(DB_QUERY_STRING)
+exports.connecter = async () => {
+    try {
+        mongoose.connection.on('open', () => console.log('DB connected'));
+        return mongoose.connect(DB_QUERY_STRING);
+    } catch (error) {
+        console.log('Error connecting to database');
+        return process.exit(1);
+    }
 };
 //or this below
 //     .then(() => {
