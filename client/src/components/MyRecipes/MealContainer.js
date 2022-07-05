efefe4ba69dbc50eef7fcd1906af0f0d18499395
Martin B <m.bogdanov9110@gmail.com>
2022-07-05 15:53:39 +0300

    Create MealContainer.js

---
+++ client/src/components/MyRecipes/MealContainer.js
@@ -0,0 +1,14 @@
+export const MealContainer = (props) => {
+
+    if (props) {
+        return (
+            <div className="meal">
+                <a href={`/details/${props._id}`}>
+                    <p className="name">{props.name}</p>
+                    <img className="meal" src={props.image} alt="" />
+                </a>
+                <a className="btn" href={`/details/${props._id}`}>Подробно</a>
+            </div>
+        );
+    }
+};
