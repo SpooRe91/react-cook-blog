41c30c44be0b0c5be7333d929da0e5ceda2f82a4
Martin B <m.bogdanov9110@gmail.com>
2022-07-05 15:54:00 +0300

    Update expressConfig.js

--- server/src/config/expressConfig.js
+++ server/src/config/expressConfig.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('../routes');
 
 module.exports = (app) => {
-    app.use('/static', express.static('public'));
     app.use(express.urlencoded({ extended: false }));//this allows the forms and search params to be displayed
+    app.use(express.json());
     app.use(routes);
 }
