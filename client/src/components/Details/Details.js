4809a38d1d80dcc845b32988ab60323574920f8a
Martin B <m.bogdanov9110@gmail.com>
2022-07-06 16:43:41 +0300

    created a fetch for details

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,31 +1,41 @@
+import { useEffect, useState } from "react";
 import { OnwerButtons } from "./OwnerButtons"
+import { API_DETAILS } from "../../API/endpoints";
 
 export const Details = () => {
 
-    const meal = {
-        name: "Mandja",
-        image: "https://images.everydayhealth.com/images/healthy-meal-tips-for-type-2-diabetes-00-722x406.jpg",
-        fullRecipe: "This is the full recipe",
-        ingredients: "These are the ingredients",
-        owner: true,
-        _id: '1234567890'
-    };
+    const [meal, setMeal] = useState();
+    const ID = "62b178243cf2539b1d58c442"
 
-    return (
-        <div className="details">
-            <h1 className="already-reg">{meal.name}</h1>
-            <a className="meal" href={meal.image} target="_blank" rel="noreferrer"><img className="meal" src={meal.image}
-                alt="" /></a>
-            {meal.owner !== undefined && meal.owner !== null
-                ? <OnwerButtons meal={meal} />
-                : <a className="btn" href="/recipe/browse">Назад</a>
-            }
-            <article className="recipe-details">
-                <label htmlFor="ingredients">Необходими съставки:</label>
-                <p className="recipe" name="ingredients"><span>{meal.ingredients}</span></p>
-                <label htmlFor="ingredients">Рецепта:</label>
-                <p className="recipe" name="ingredients"><span>{meal.fullRecipe}</span></p>
-            </article>
-        </div>
-    );
+    useEffect(() => {
+        fetch(API_DETAILS(ID))
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setMeal(data)
+            })
+            .catch(error => {
+                throw error.message
+            })
+    }, [])
+
+    if (meal) {
+        return (
+            <div className="details">
+                <h1 className="already-reg">{meal.name}</h1>
+                <a className="meal" href={meal.image} target="_blank" rel="noreferrer"><img className="meal" src={meal.image}
+                    alt="" /></a>
+                {meal.owner !== undefined && meal.owner !== null
+                    ? <OnwerButtons meal={meal} />
+                    : <a className="btn" href="/recipe/browse">Назад</a>
+                }
+                <article className="recipe-details">
+                    <label htmlFor="ingredients">Необходими съставки:</label>
+                    <p className="recipe" name="ingredients"><span>{meal.ingredients}</span></p>
+                    <label htmlFor="ingredients">Рецепта:</label>
+                    <p className="recipe" name="ingredients"><span>{meal.fullRecipe}</span></p>
+                </article>
+            </div>
+        );
+    }
 }
