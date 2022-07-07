b1a3c1328f8aed1192f66902484e12701fda6904
Martin B <m.bogdanov9110@gmail.com>
2022-07-07 23:30:02 +0300

    update on cors middleware

--- server/src/middlewares/corsMiddleware.js
+++ server/src/middlewares/corsMiddleware.js
@@ -1,7 +1,9 @@
 module.exports = () => (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, X-Authorization, x-requested-with');
 
+    res.setHeader('Access-Control-Allow-Origin', "*");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Authorization, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
     next();
+
 };
