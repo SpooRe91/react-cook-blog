eeba63ff8f198f3ac5e0bd1dcf04f267ba5005f4
Martin B <m.bogdanov9110@gmail.com>
2022-07-07 23:29:39 +0300

    update

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -4,7 +4,7 @@ import { endpoints } from "../../API/endpoints";
 
 export const Details = () => {
 
-    const [meal, setMeal] = useState();
+    const [meal, setMeal] = useState({});
     const ID = "62b178243cf2539b1d58c442"
 
     useEffect(() => {
@@ -19,23 +19,21 @@ export const Details = () => {
             })
     }, [])
 
-    if (meal) {
-        return (
-            <div className="details">
-                <h1 className="already-reg">{meal.name}</h1>
-                <a href={meal.image} target="_blank" rel="noreferrer"><img className="meal" src={meal.image}
-                    alt="" /></a>
-                {meal.owner !== undefined && meal.owner !== null
-                    ? <OnwerButtons meal={meal} />
-                    : <a className="btn" href="/recipe/browse">Назад</a>
-                }
-                <article className="recipe-details">
-                    <label htmlFor="ingredients">Необходими съставки:</label>
-                    <p className="recipe" name="ingredients"><span>{meal.ingredients}</span></p>
-                    <label htmlFor="ingredients">Рецепта:</label>
-                    <p className="recipe" name="ingredients"><span>{meal.fullRecipe}</span></p>
-                </article>
-            </div>
-        );
-    }
+    return (
+        <div className="details">
+            <h1 className="meal-name">{meal.name}</h1>
+            <a href={meal.image} target="_blank" rel="noreferrer"><img className="meal" src={meal.image}
+                alt="" /></a>
+            {meal.owner !== undefined && meal.owner !== null
+                ? <OnwerButtons meal={meal} />
+                : <a className="btn" href="/recipe/browse">Назад</a>
+            }
+            <article className="recipe-details">
+                <label htmlFor="ingredients">Необходими съставки:</label>
+                <p className="recipe" name="ingredients"><span>{meal.ingredients}</span></p>
+                <label htmlFor="ingredients">Рецепта:</label>
+                <p className="recipe" name="ingredients"><span>{meal.fullRecipe}</span></p>
+            </article>
+        </div>
+    );
 }
