f0e7ef87614d734dd7ffdaeb977daf39363f146e
Martin B <m.bogdanov9110@gmail.com>
2022-07-08 22:44:08 +0300

    Update About.js

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -4,7 +4,7 @@ export const About = () => {
         <div className="already-reg">
 
             <h1 className="already-reg">Martin Bogdanov</h1>
-            <a href="https://github.com/SpooRe91"><img
+            <a href="https://github.com/SpooRe91" target="_blank" rel="noreferrer"><img
                 src="https://avatars.githubusercontent.com/u/85784810?s=400&u=575287644400378b5bfb641dd8484aa03d2fe063&v=4"
                 alt="#" /></a>
             <h3>About me:</h3>
@@ -12,7 +12,7 @@ export const About = () => {
                 Hello, my name is Martin, I am a former professional cook and a learning programmer. Below is my GitHub
                 profile, where you can find few interesting ongoing and finished projects.
             </p>
-            <h4>My GitHub:<a href="https://github.com/SpooRe91">*click here*</a></h4>
+            <h4>My GitHub:<a href="https://github.com/SpooRe91" target="_blank" rel="noreferrer">*click here*</a></h4>
 
         </div >);
 }
