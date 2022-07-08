ac4f8435de4149846ae4ea6dc6f2ba0e6775a9c1
Martin B <m.bogdanov9110@gmail.com>
2022-07-08 22:44:05 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { Route, Routes } from "react-router-dom"
 import { Login } from "./components/Login/Login"
 import { NavBar } from "./components/NavBars/Navbar"
 import { Register } from "./components/Register/Register";
@@ -8,21 +9,29 @@ import { AddRecipe } from "./components/AddRecipe/AddRecipe";
 import { Browse } from "./components/Browse/Browse";
 import { Details } from "./components/Details/Details";
 import { MyRecipes } from "./components/MyRecipes/MyRecipes";
+import { Homepage } from "./components/Homepage/Homepage";
+import { Footer } from "./components/Footer/Footer";
+import { Header } from "./components/Header/Header";
 
 function App() {
 
   return (
     <div className="App">
+      <Header />
       <NavBar />
-      <Login />
-      <Register />
-      <About />
-      <Contacts />
-      <ErrorPage />
-      <AddRecipe />
-      <Browse />
-      <Details/>
-      <MyRecipes />
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/auth/login" element={<Login />} />
+        <Route path="/auth/register" element={<Register />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/404" element={<ErrorPage />} />
+        <Route path="/" element={<AddRecipe />} />
+        <Route path="/recipe/browse" element={<Browse />} />
+        <Route path="/details/:userId" element={<Details />} />
+        <Route path="/" element={<MyRecipes />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
