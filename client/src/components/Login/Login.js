7877011b63ae5cdc4e66e6d11e9d35184a1fd410
Martin B <m.bogdanov9110@gmail.com>
2022-07-09 02:36:34 +0300

    Update Login.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,9 +1,29 @@
+import { useState } from "react";
+import { userLogin } from "../../services/userService";
+
 export const Login = () => {
+
+    const [user, setUser] = useState({});
+
+    const onLoginHandler = (e) => {
+
+        e.preventDefault();
+
+        const formData = new FormData(e.target);
+        const { email, password } = Object.fromEntries(formData);
+
+        
+        userLogin({ email, password })
+            .then(res => {
+                setUser(res)
+            });
+        return user;
+    };
 
     return (
         <div className="form">
             <h3 className="already-reg">Влизане</h3>
-            <form method="POST">
+            <form method="POST" onSubmit={onLoginHandler}>
                 <label className="already-reg" htmlFor="email">e-mail</label>
                 <input type="text" className="email" id="email" name="email" placeholder="e-mail..." required />
 
