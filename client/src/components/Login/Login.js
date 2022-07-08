46dafbb7b0738f9eaa6ce016c4216417cf33fd20
Martin B <m.bogdanov9110@gmail.com>
2022-07-08 22:44:22 +0300

    Update Login.js

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,18 +1,19 @@
 export const Login = () => {
 
-    return (<div className="form">
-        <form method="POST">
+    return (
+        <div className="form">
+            <h3 className="already-reg">Влизане</h3>
+            <form method="POST">
+                <label className="already-reg" htmlFor="email">e-mail</label>
+                <input type="text" className="email" id="email" name="email" placeholder="e-mail..." required />
 
-            <label className="already-reg" htmlFor="email">e-mail</label>
-            <input type="text" id="email" name="email" placeholder="e-mail..." required />
+                <label className="already-reg" htmlFor="password">парола</label>
+                <input type="password" className="password" id="password" name="password" placeholder="парола..." required />
 
-            <label className="already-reg" htmlFor="password">парола</label>
-            <input type="password" id="password" name="password" placeholder="парола..." required />
+                <input className="already-reg" type="submit" value="Вход" />
+            </form>
 
-            <input className="already-reg" type="submit" value="Вход" />
-        </form>
-        
             <h3 className="already-reg">Нямате регистрация? <a href="/auth/register">Регистрирайте се тук!</a></h3>
-    </div>);
+        </div>);
 }
 
