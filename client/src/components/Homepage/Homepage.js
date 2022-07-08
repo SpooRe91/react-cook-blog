1d7a0abcc412045d30a90caa0f0958950b0c0b0f
Martin B <m.bogdanov9110@gmail.com>
2022-07-08 22:44:19 +0300

    Create Homepage.js

---
+++ client/src/components/Homepage/Homepage.js
@@ -0,0 +1,13 @@
+export const Homepage = () => {
+
+    return (
+        <article className="landing-page-article">
+            <div>
+                <section className="landing-page-section">
+                    <h1 ><i className="landing-page-header">Welcome to Cook-Blog, your favourite meals await you!</i></h1>
+                </section>
+            </div>
+        </article>
+    );
+
+}
