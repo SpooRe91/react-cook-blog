1a85b96da02c201fa834402cdd41d221e54707b1
Martin B <m.bogdanov9110@gmail.com>
2022-07-09 17:10:53 +0300

    updated browse

--- client/src/components/Browse/Browse.js
+++ client/src/components/Browse/Browse.js
@@ -1,5 +1,20 @@
+import { useEffect, useState } from "react";
+import { endpoints } from "../../API/endpoints";
+import { getAll } from "../../services/mealService";
 import { Meal } from "./Meal"
-export const Browse = (props) => {
+
+export const Browse = () => {
+
+    const [meals, setMeals] = useState([]);
+
+    useEffect(() => {
+        getAll(endpoints.API_BROWSE)
+            .then(res => {
+                console.log(res);
+                setMeals(res)
+            })
+    }, []);
+
     return (
         <div className="search-container">
             <form method="GET">
@@ -8,8 +23,8 @@ export const Browse = (props) => {
             </form>
 
             <div className="meal-containter">
-                {props.meal !== undefined && props.meal !== null
-                    ? <Meal meal={props.meal} />
+                {meals !== undefined && meals !== null && meals.length > 0
+                    ? meals.map(meal => <Meal key={meal._id} {...meal} />)
                     : <div className="already-reg">
                         <p>Все още няма рецепти!</p>
                     </div>
