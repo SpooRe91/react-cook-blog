273dbb3a063621f9dc120eb8471cd97c6d7c7c2e
Martin B <m.bogdanov9110@gmail.com>
2022-07-09 17:14:57 +0300

    update

--- client/src/services/mealService.js
+++ client/src/services/mealService.js
@@ -1,8 +1,36 @@
 
-export const getOneById = async (endpoint) => {
+export const getOne = async (endpoint) => {
 
     try {
         const res = await fetch(endpoint);
+        return await res.json();
+    } catch (error) {
+        throw new Error(error.message)
+    }
+};
+
+export const getAll = async (endpoint) => {
+
+    try {
+        const res = await fetch(endpoint);
+        return await res.json();
+    } catch (error) {
+        throw new Error(error.message)
+    }
+};
+
+export const getOwn = async (endpoint) => {
+
+    try {
+        const res = await fetch(endpoint, {
+            mode: 'cors',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+                'Allow-Control-Access-Policy': true,
+                "Access-Control-Allow-Credentials": true,
+            }
+        });
         return await res.json();
     } catch (error) {
         throw new Error(error.message)
