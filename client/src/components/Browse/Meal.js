4526ab770e2e99c91c3948dfbf88e8a1e023132d
Martin B <m.bogdanov9110@gmail.com>
2022-07-09 17:11:12 +0300

    updated Meal componen

--- client/src/components/Browse/Meal.js
+++ client/src/components/Browse/Meal.js
@@ -1,11 +1,11 @@
-export const Meal = (props) => {
+export const Meal = (meal) => {
     return (
         <div className="meal">
             <a href="/details/{{_id}}">
-                <p className="name">{props.meal.name}</p>
-                <img className="meal" src={props.meal.image} alt="" />
+                <p className="name">{meal.name}</p>
+                <img className="meal" src={meal.image} alt="" />
             </a>
-            <a className="btn" href={`/details/${props.meal._id}`}>Подробно</a>
+            <a className="btn" href={`/details/${meal._id}`}>Подробно</a>
         </div>
     );
 }
