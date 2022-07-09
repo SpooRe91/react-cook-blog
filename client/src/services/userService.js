df940933f2a31a5fb92672b0d6408e9575e609f9
Martin B <m.bogdanov9110@gmail.com>
2022-07-09 17:15:12 +0300

    created logout

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,3 +1,4 @@
+import { logoutSession } from "../API/api";
 import { endpoints } from "../API/endpoints";
 
 export const userLogin = async (loginData) => {
@@ -6,6 +7,7 @@ export const userLogin = async (loginData) => {
         const loginResult = await fetch(endpoints.API_LOGIN, {
             method: 'POST',
             mode: 'cors',
+            credentials: "include",
             headers: {
                 'Content-Type': 'application/json',
                 'Allow-Control-Access-Policy': true,
@@ -15,6 +17,23 @@ export const userLogin = async (loginData) => {
         });
         return await loginResult.json();
     } catch (error) {
-        throw new Error(error.message)
+        throw new Error(error.message);
     }
 };
+
+export const userLogout = async () => {
+    try {
+        await fetch(endpoints.API_LOGOUT, {
+            mode: "cors",
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Allow-Control-Access-Policy': true,
+                "Access-Control-Allow-Credentials": true,
+            }
+        });
+        logoutSession();
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
