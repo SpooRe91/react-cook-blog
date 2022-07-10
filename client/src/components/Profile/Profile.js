42f2f6fc9568bd7d2897a5cc7f8ebda4eadf513c
Martin B <m.bogdanov9110@gmail.com>
2022-07-10 04:31:51 +0300

    added the Profile.js

---
+++ client/src/components/Profile/Profile.js
@@ -0,0 +1,40 @@
+export const Profile = () => {
+    return (
+        <div classList="already-reg"><a classList="maia-button maia-button-primary" style={{ "float": "right" }} href="./edit-profile.g">Edit Profile</a>
+            <h1>mb_cookinglife</h1>
+            <div ><p style={{ "text-align": "center" }}><img classList="photo" src="//4.bp.blogspot.com/-2adU6pDpCx8/XslRaR-vDZI/AAAAAAAAA8c/jgR5KKcPVKomQJrFW1FK9u4MmsmiNy14QCK4BGAYYCw/s113/received_418083412301655.jpeg" style={{ "width": "140px", "max-height": "280px" }} id="profile-photo" alt="#" /></p>
+                <p style={{ "text-align": "center" }}><a href="//4.bp.blogspot.com/-2adU6pDpCx8/XslRaR-vDZI/AAAAAAAAA8c/jgR5KKcPVKomQJrFW1FK9u4MmsmiNy14QCK4BGAYYCw/s113/received_418083412301655.jpeg">View Full Size</a></p>
+                <div >
+                    <div></div>
+                    <p>On Blogger since
+                        September 2014</p>
+                    <div></div>
+                    <p>Profile views
+                        -
+                        58</p></div></div>
+            <div classList="contents-after-sidebar" style={{ "padding-left": "245px", "min-height": "600px" }}><h2>My blogs</h2>
+                <ul><li classList="sidebar-item"><span dir="ltr"><a href="http://mbcookinglife.blogspot.com/" target="null" rel="contributor-to nofollow" onclick="">Martin's cooking life</a></span></li></ul>
+                <div classList="section-divider"></div>
+
+                <h2>About me</h2><table>
+                    <tbody><tr><th classList="item-key">Gender</th>
+                        <td>MALE</td></tr>
+                        <tr><th classList="item-key">Industry</th>
+                            <td><span classList="role">Tourism</span></td></tr>
+                        <tr><th classList="item-key">Occupation</th>
+                            <td><span classList="title">Cook/Chef</span></td></tr>
+                        <tr><th classList="item-key">Location</th>
+                            <td><span classList="locality">Varna,</span>
+
+                                <span classList="country-name">Bulgaria</span></td></tr>
+
+                        <tr><th classList="item-key">Introduction</th>
+                            <td>My name is Martin, I am a professional chef, and I would like to share some of my favorite dishes with you.</td></tr>
+                        <tr><th classList="item-key">Interests</th>
+                            <td><span classList="favorites">Cooking, Sports, Movies, PC Games, Music</span></td></tr>
+                    </tbody></table>
+            </div>
+        </div>
+    )
+}
+
