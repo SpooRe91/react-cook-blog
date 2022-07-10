838cba31105dfef9d077d9ae0def176399bc8b34
Martin B <m.bogdanov9110@gmail.com>
2022-07-10 15:53:35 +0300

    update

--- client/src/components/Logout/Logout.js
+++ client/src/components/Logout/Logout.js
@@ -1,5 +1,6 @@
 export const Logout = (props) => {
 
+    console.log(props);
     return (
         <div className="already-reg">
             <p>Излязохте успешно! <a href="/" className="already-reg">HAЧАЛНА СТРАНИЦА</a></p>
