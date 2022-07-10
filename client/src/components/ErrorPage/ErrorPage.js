0ead5db56a60758ae3154c6c9bfe2212e6655b22
Martin B <m.bogdanov9110@gmail.com>
2022-07-10 15:53:26 +0300

    updated ErrorPage.js

--- client/src/components/ErrorPage/ErrorPage.js
+++ client/src/components/ErrorPage/ErrorPage.js
@@ -1,8 +1,9 @@
 export const ErrorPage = (props) => {
+    console.log(props);
     return (
         <div className="container">
             <main>
-                <h1 className="error-message">404 Not found!</h1>
+                <h1 className="error-message">Error</h1>
                 {props.error && <h1 className="error-message">{props.error.message}</h1>}
             </main>
         </div>
