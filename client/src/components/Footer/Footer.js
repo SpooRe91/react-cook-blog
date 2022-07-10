27510e14bdf992f3a829bd781be3f6ec80085335
Martin B <m.bogdanov9110@gmail.com>
2022-07-11 02:45:15 +0300

    updated footer with logic for guests and users

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,13 +1,23 @@
 import { Link } from "react-router-dom";
 
-export const Footer = () => {
+export const Footer = ({ setIsOpen, user }) => {
+    console.log(user);
+    
     return (
         <footer className="footer">
-            <p>Copyright © designed by <a href="https://github.com/SpooRe91" className="btn-footer-created-by">Martin Bogdanov</a> </p>
-            <Link to="/about" className="btn-footer">Относно</Link>
-            <Link to="/contacts" className="btn-footer">Контакти</Link>
-            <Link to="/auth/register" className="btn-footer">Регистрация</Link>
-            <Link to="/auth/login" className="btn-footer">Вход</Link>
-        </footer>
+            <p>Copyright © designed by <a href="https://github.com/SpooRe91" target={"_blank"} rel="noreferrer" className="btn-footer-created-by">Martin Bogdanov</a> </p>
+            {user !== null && user.id
+                ? (<>
+                    <Link to="#" className="btn-footer" name="about" onClick={(e) => { setIsOpen({ state: true, target: e.target.name }) }}>Относно</Link>
+                    <Link to="#" className="btn-footer" name="contacts" onClick={(e) => { setIsOpen({ state: true, target: e.target.name }) }}>Контакти</Link>
+                </>)
+                : (<>
+                    <Link to="#" className="btn-footer" name="about" onClick={(e) => { setIsOpen({ state: true, target: e.target.name }) }}>Относно</Link>
+                    <Link to="#" className="btn-footer" name="contacts" onClick={(e) => { setIsOpen({ state: true, target: e.target.name }) }}>Контакти</Link>
+                    <Link to="/auth/register" className="btn-footer">Регистрация</Link>
+                    <Link to="/auth/login" className="btn-footer" >Вход</Link>
+                </>)
+            }
+        </footer >
     );
 }
