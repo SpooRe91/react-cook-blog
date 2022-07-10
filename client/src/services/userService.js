f6f8c89ed8b8dc9d8ae4f43bb192697980c635c4
Martin B <m.bogdanov9110@gmail.com>
2022-07-10 15:55:34 +0300

    added userRegister

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,5 +1,24 @@
-import { logoutSession } from "../API/api";
 import { endpoints } from "../API/endpoints";
+
+export const userRegister = async (registerData) => {
+
+    try {
+        const registerResult = await fetch(endpoints.API_REGISTER, {
+            method: 'POST',
+            mode: 'cors',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+                'Allow-Control-Access-Policy': true,
+                "Access-Control-Allow-Credentials": true,
+            },
+            body: JSON.stringify(registerData),
+        });
+        return await registerResult.json();
+    } catch (error) {
+        throw new Error(error.message);
+    }
+}
 
 export const userLogin = async (loginData) => {
 
@@ -22,6 +41,7 @@ export const userLogin = async (loginData) => {
 };
 
 export const userLogout = async () => {
+
     try {
         await fetch(endpoints.API_LOGOUT, {
             mode: "cors",
@@ -32,7 +52,6 @@ export const userLogout = async () => {
                 "Access-Control-Allow-Credentials": true,
             }
         });
-        logoutSession();
     } catch (error) {
         throw new Error(error.message);
     }
