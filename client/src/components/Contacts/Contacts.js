e0493c0e55635da710bc92b1a2268ec06070b1d2
Martin B <m.bogdanov9110@gmail.com>
2022-07-10 17:02:18 +0300

    update

--- client/src/components/Contacts/Contacts.js
+++ client/src/components/Contacts/Contacts.js
@@ -3,14 +3,15 @@ export const Contacts = () => {
     return (
         <div className="already-reg">
 
-            <h1 className="already-reg">Martin Bogdanov</h1>
-            <a href="https://github.com/SpooRe91" target="_blank" rel="noreferrer"><img
-                src="https://avatars.githubusercontent.com/u/85784810?s=400&u=575287644400378b5bfb641dd8484aa03d2fe063&v=4"
-                alt="#" /></a>
-            <h3>About me:</h3>
+            <h1 className="already-reg">About me:</h1>
+
+            <h3 className="already-reg">Martin Bogdanov</h3>
             <p className="already-reg">
                 Hello, my name is Martin, I am a former professional cook and a learning programmer. Below is my GitHub
                 profile, where you can find few interesting ongoing and finished projects.
+            </p>
+            <p className="already-reg">
+                You can also reach me on my e-mail:<strong>m.bogdanov9110@gmail.com</strong>
             </p>
             <h4>My GitHub:<a href="https://github.com/SpooRe91" target="_blank" rel="noreferrer">*click here*</a></h4>
 
