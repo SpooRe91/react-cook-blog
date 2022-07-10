f74eff82b84554b211ce6b81ab1f275134fbdd3a
Martin B <m.bogdanov9110@gmail.com>
2022-07-10 15:54:36 +0300

    updated the JSX on Profile.js

--- client/src/components/Profile/Profile.js
+++ client/src/components/Profile/Profile.js
@@ -1,38 +1,33 @@
 export const Profile = () => {
     return (
-        <div classList="already-reg"><a classList="maia-button maia-button-primary" style={{ "float": "right" }} href="./edit-profile.g">Edit Profile</a>
-            <h1>mb_cookinglife</h1>
-            <div ><p style={{ "text-align": "center" }}><img classList="photo" src="//4.bp.blogspot.com/-2adU6pDpCx8/XslRaR-vDZI/AAAAAAAAA8c/jgR5KKcPVKomQJrFW1FK9u4MmsmiNy14QCK4BGAYYCw/s113/received_418083412301655.jpeg" style={{ "width": "140px", "max-height": "280px" }} id="profile-photo" alt="#" /></p>
-                <p style={{ "text-align": "center" }}><a href="//4.bp.blogspot.com/-2adU6pDpCx8/XslRaR-vDZI/AAAAAAAAA8c/jgR5KKcPVKomQJrFW1FK9u4MmsmiNy14QCK4BGAYYCw/s113/received_418083412301655.jpeg">View Full Size</a></p>
-                <div >
-                    <div></div>
-                    <p>On Blogger since
-                        September 2014</p>
-                    <div></div>
-                    <p>Profile views
-                        -
-                        58</p></div></div>
-            <div classList="contents-after-sidebar" style={{ "padding-left": "245px", "min-height": "600px" }}><h2>My blogs</h2>
-                <ul><li classList="sidebar-item"><span dir="ltr"><a href="http://mbcookinglife.blogspot.com/" target="null" rel="contributor-to nofollow" onclick="">Martin's cooking life</a></span></li></ul>
-                <div classList="section-divider"></div>
+        <div className="already-reg"><a className="maia-button maia-button-primary" href="#/">Edit Profile</a>
+            <h1 className="already-reg">Martin Bogdanov</h1>
+            <div><a><img className="already-reg" src="https://avatars.githubusercontent.com/u/85784810?s=400&u=575287644400378b5bfb641dd8484aa03d2fe063&v=4" id="profile-photo" alt="#" /></a>
+                <p><a href="https://avatars.githubusercontent.com/u/85784810?s=400&u=575287644400378b5bfb641dd8484aa03d2fe063&v=4">View Full Size</a></p>
+                <div>
+                    <div className="contents-after-sidebar"><h2>My blogs</h2>
+                        <ul><li className="sidebar-item"><span dir="ltr"><a href="http://mbcookinglife.blogspot.com/" target="null" rel="contributor-to nofollow">Martin's cooking life</a></span></li></ul>
+                        <div className="section-divider"></div>
 
-                <h2>About me</h2><table>
-                    <tbody><tr><th classList="item-key">Gender</th>
-                        <td>MALE</td></tr>
-                        <tr><th classList="item-key">Industry</th>
-                            <td><span classList="role">Tourism</span></td></tr>
-                        <tr><th classList="item-key">Occupation</th>
-                            <td><span classList="title">Cook/Chef</span></td></tr>
-                        <tr><th classList="item-key">Location</th>
-                            <td><span classList="locality">Varna,</span>
+                        <h2>About me</h2><table>
+                            <tbody><tr><th className="item-key">Gender</th>
+                                <td>MALE</td></tr>
+                                <tr><th className="item-key">Industry</th>
+                                    <td><span className="role">Tourism</span></td></tr>
+                                <tr><th className="item-key">Occupation</th>
+                                    <td><span className="role">Cook/Chef</span></td></tr>
+                                <tr><th className="item-key">Location</th>
+                                    <td><span className="locality">Varna,</span>
 
-                                <span classList="country-name">Bulgaria</span></td></tr>
+                                        <span className="country-name">Bulgaria</span></td></tr>
 
-                        <tr><th classList="item-key">Introduction</th>
-                            <td>My name is Martin, I am a professional chef, and I would like to share some of my favorite dishes with you.</td></tr>
-                        <tr><th classList="item-key">Interests</th>
-                            <td><span classList="favorites">Cooking, Sports, Movies, PC Games, Music</span></td></tr>
-                    </tbody></table>
+                                <tr><th className="item-key">Introduction</th>
+                                    <td>My name is Martin, I am a professional chef, and I would like to share some of my favorite dishes with you.</td></tr>
+                                <tr><th className="item-key">Interests</th>
+                                    <td><span className="favorites">Cooking, Sports, Movies, PC Games, Music</span></td></tr>
+                            </tbody></table>
+                    </div>
+                </div>
             </div>
         </div>
     )
