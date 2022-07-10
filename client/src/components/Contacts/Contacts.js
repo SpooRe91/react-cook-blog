50e73db1f9543952bc1bbb185a9f2a072663ec3a
Martin B <m.bogdanov9110@gmail.com>
2022-07-11 02:44:51 +0300

    created a modal for Contacts and a CSS module (needs to be finished)

--- client/src/components/Contacts/Contacts.js
+++ client/src/components/Contacts/Contacts.js
@@ -1,19 +1,32 @@
-export const Contacts = () => {
+import styles from "./Contacts.module.css"
+
+export const Contacts = ({ setIsOpen }) => {
 
     return (
-        <div className="already-reg">
+        <>
+            <div className={styles.darkBG} onClick={() => setIsOpen(false)} />
+            <div className={styles.centered}>
+                <div className="already-reg">
 
-            <h1 className="already-reg">About me:</h1>
+                    <h1 className="already-reg">Контакти и кратка биография</h1>
 
-            <h3 className="already-reg">Martin Bogdanov</h3>
-            <p className="already-reg">
-                Hello, my name is Martin, I am a former professional cook and a learning programmer. Below is my GitHub
-                profile, where you can find few interesting ongoing and finished projects.
-            </p>
-            <p className="already-reg">
-                You can also reach me on my e-mail:<strong>m.bogdanov9110@gmail.com</strong>
-            </p>
-            <h4>My GitHub:<a href="https://github.com/SpooRe91" target="_blank" rel="noreferrer">*click here*</a></h4>
-
-        </div >);
+                    <h3 className="already-reg">Martin Bogdanov</h3>
+                    <p className="already-reg">
+                        Казвам се Мартин, аз съм бивш професионален готвач и по настоящем учащ се софтуерен инженер.
+                        По-долу, можете да намерите линк към моята GitHub страница, където има най-различни започнати, готови или проекти в процес на разработка.
+                    </p>
+                    <p className="already-reg">
+                        Съшо така, можете да се свържете с мен чрез e-mail на:<strong>m.bogdanov9110@gmail.com</strong>
+                    </p>
+                    <h4>Линк към GitHub:<a href="https://github.com/SpooRe91" target="_blank" rel="noreferrer">ТУК</a></h4>
+                    <div>
+                        {/*button which closes the modal, when clicked */}
+                        <input type="button" className="already-reg"
+                            onClick={() => setIsOpen(false)}
+                            value="Cancel" />
+                    </div>
+                </div >
+            </div >
+        </>
+    );
 }
