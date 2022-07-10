21fb1782c24e9f0d6c4d433f5e5596c5bb6c0b48
Martin B <m.bogdanov9110@gmail.com>
2022-07-11 02:44:31 +0300

    created modal for About and a css module for it (needs adding of the rest(

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,16 +1,30 @@
-export const About = () => {
+import styles from './About.module.css'
+
+export const About = ({ setIsOpen }) => {
     return (
-        <div className="already-reg">
-            <h1 className="already-reg">Cook-blog by Martin Bogdanov about page</h1>
-            <p className="about-text">
-                "Cook-blog" is a basic SPA, created with React-app.
-                It allows for CRUD operations by making AJAX requests to a custom REST Api. It was written by myself using with Express.js.
-                The back-end is running on Node.js.
-            </p>
-            <p className="about-text">
-                Please enjoy the SPA and don't hesitate to <strong>contact</strong> me for further
-                suggestions at <span> <strong>m.bogdanov9110@gmail.com</strong></span>, or at:
-                <a href="/about">Martin Bogdanov</a>
-            </p>
-        </div >);
+        <>
+            <div className={styles.darkBG} onClick={() => setIsOpen(false)} />
+            <div className={styles.centered}>
+                <div className="already-reg">
+                    <h1 className="already-reg">Относно <span>"Cook-blog by Martin Bogdanov"</span></h1>
+                    <p className="about-text">
+                        "Cook-blog" is a basic SPA, created with React-app.
+                        It allows for CRUD operations by making AJAX requests to a custom REST Api. It was written by myself using with Express.js.
+                        The back-end is running on Node.js.
+                    </p>
+                    <p className="about-text">
+                        Please enjoy the SPA and don't hesitate to <strong>contact</strong> me for further
+                        suggestions at <span> <strong>m.bogdanov9110@gmail.com</strong></span>
+                    </p>
+                    <div>
+                        {/*button which closes the modal, when clicked */}
+                        <input type="button" className="already-reg"
+                            onClick={() => setIsOpen(false)}
+                            value="Cancel" />
+                    </div>
+                </div>
+
+            </div>
+        </>
+    );
 }
