749cd336f2a364915fcfae459928f19b72d0065e
Martin B <m.bogdanov9110@gmail.com>
2022-07-10 17:02:25 +0300

    update

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 export const Footer = () => {
     return (
         <footer className="footer">
-            <p>Copyright © designed by Martin Bogdanov</p>
+            <p>Copyright © designed by <a href="https://github.com/SpooRe91" className="btn-footer-created-by">Martin Bogdanov</a> </p>
             <Link to="/about" className="btn-footer">Относно</Link>
             <Link to="/contacts" className="btn-footer">Контакти</Link>
+            <Link to="/auth/register" className="btn-footer">Регистрация</Link>
+            <Link to="/auth/login" className="btn-footer">Вход</Link>
         </footer>
     );
 }
