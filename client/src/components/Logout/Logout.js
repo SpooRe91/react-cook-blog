a19d8f83fb8958eb951943d6952967f0c7b23cba
Martin B <m.bogdanov9110@gmail.com>
2022-07-11 02:45:26 +0300

    Delete Logout.js

--- client/src/components/Logout/Logout.js
+++
@@ -1,9 +0,0 @@
-export const Logout = (props) => {
-
-    console.log(props);
-    return (
-        <div className="already-reg">
-            <p>Излязохте успешно! <a href="/" className="already-reg">HAЧАЛНА СТРАНИЦА</a></p>
-        </div>
-    )
-}
