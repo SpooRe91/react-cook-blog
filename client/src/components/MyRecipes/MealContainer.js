0233baaff7bc5968668e74fa8b23c6a1b18dd554
Martin B <m.bogdanov9110@gmail.com>
2022-07-12 00:18:50 +0300

    Update MealContainer.js

--- client/src/components/MyRecipes/MealContainer.js
+++ client/src/components/MyRecipes/MealContainer.js
@@ -1,14 +1,17 @@
+import { Link } from "react-router-dom";
+
 export const MealContainer = (props) => {
 
     return (
-        <div className="meal">
-            <a href={`/details/${props._id}`}>
-                <p className="name">{props.name}</p>
-            </a>
-            <a href={`/details/${props._id}`}>
-                <img className="meal" src={props.image} alt="" />
-            </a>
-            <a className="btn" href={`/details/${props._id}`}>Подробно</a>
-        </div >
+        <>
+            <div className="meal">
+                <Link to={`/details/${props._id}`} >
+                    <p className="name">{props.name}</p>
+                </Link>
+                <Link to={`/details/${props._id}`} >
+                    <img className="meal" src={props.image} alt="" /></Link>
+                <Link className="btn" to={`/details/${props._id}`}>Подробно</Link>
+            </div >
+        </>
     );
 };
