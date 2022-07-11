78ef8d35522008474b6de1cfbd476e037cf8cace
Martin B <m.bogdanov9110@gmail.com>
2022-07-12 00:18:46 +0300

    Update OwnerButtons.js

--- client/src/components/Details/OwnerButtons.js
+++ client/src/components/Details/OwnerButtons.js
@@ -1,9 +1,10 @@
+import { Link } from "react-router-dom"
 export const OnwerButtons = (props) => {
     return (
         <>
-            <a className="btn" href={`/edit/${props.meal._id}`}>Промени</a>
-            <a className="btn" href={`/delete/${props.meal._id}`}>Изтрии</a>
-            <a className="btn" href="/recipe/browse">Назад</a>
+            <Link className="btn" to={`/edit/${props.meal._id}`}>Промени</Link>
+            <Link className="btn" to={`/delete/${props.meal._id}`}>Изтрии</Link>
+            <Link className="btn" to="/recipe/browse">Назад</Link>
         </>
     );
 }
