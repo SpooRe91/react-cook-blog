2b7976d8f21c94424d462aff88b0688e58ea87bd
Martin B <m.bogdanov9110@gmail.com>
2022-07-11 19:07:23 +0300

    updated Login

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { getSession, setSession } from "../../API/api";
 import { userLogin } from "../../services/userService";
