86c88687d014d0729b32bb6dd16005a793d7f4a9
Martin B <m.bogdanov9110@gmail.com>
2022-07-13 01:59:13 +0300

    added a useCallback hook

--- client/src/components/NavBars/Navbar.js
+++ client/src/components/NavBars/Navbar.js
@@ -3,9 +3,14 @@ import { GuestNavBar } from "./GuestNavBar";
 import { UserNavBar } from "./UserNavBar";
 import logo from './logo.jpg';
 import { Link } from "react-router-dom";
+import { useCallback } from 'react';
+import styles from "./NavBar.module.css"
 
 
 export const NavBar = ({ user, setUser, setIsOpen, clientCookie }) => {
+
+    const focusHandler = useCallback(({ isActive }) => isActive ? styles['active-element'] : "");
+
     return (
         <nav>
             <ul>
@@ -14,8 +19,9 @@ export const NavBar = ({ user, setUser, setIsOpen, clientCookie }) => {
                 </li>
             </ul>
             {user !== null && user.id && clientCookie !== undefined
-                ? <UserNavBar {...user} setUser={setUser} setIsOpen={setIsOpen} />
-                : <GuestNavBar />
+                ? <UserNavBar {...user} setUser={setUser} setIsOpen={setIsOpen} focusHandler={focusHandler} />
+                : <GuestNavBar focusHandler={focusHandler} />
             }
         </nav >);
-} 
+
+}
