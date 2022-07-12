ee2da1aa41d9e26208d30338795752545ae10540
Martin B <m.bogdanov9110@gmail.com>
2022-07-12 22:57:51 +0300

    Update Navbar.js

--- client/src/components/NavBars/Navbar.js
+++ client/src/components/NavBars/Navbar.js
@@ -1,9 +1,18 @@
-import { GuestNavBar } from "./GuestNavBar"
-import { UserNavBar } from "./UserNavBar"
+
+import { GuestNavBar } from "./GuestNavBar";
+import { UserNavBar } from "./UserNavBar";
+import logo from './logo.jpg';
+import { Link } from "react-router-dom";
+
 
 export const NavBar = ({ user, setUser, setIsOpen, clientCookie }) => {
     return (
         <nav>
+            <ul>
+                <li>
+                    <Link to="/"><img className="nav-logo" src={logo} alt="#" /></Link>
+                </li>
+            </ul>
             {user !== null && user.id && clientCookie !== undefined
                 ? <UserNavBar {...user} setUser={setUser} setIsOpen={setIsOpen} />
                 : <GuestNavBar />
