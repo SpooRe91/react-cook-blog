baaa0f492cbbd65cd53a825fce6c28266d21006c
Martin B <m.bogdanov9110@gmail.com>
2022-07-14 02:55:19 +0300

    css updates

--- client/src/components/MyRecipes/MealContainer.js
+++ client/src/components/MyRecipes/MealContainer.js
@@ -8,7 +8,7 @@ export const MealContainer = (props) => {
                 <Link to={`/details/${props._id}`} >
                     <p className="name">{props.name}</p>
                 </Link>
-                <Link to={`/details/${props._id}`} >
+                <Link to={`/details/${props._id}`} className="meal-image-link">
                     <img className="meal" src={props.image} alt="" /></Link>
                 <Link className="btn" to={`/details/${props._id}`}>Подробно</Link>
             </div >
