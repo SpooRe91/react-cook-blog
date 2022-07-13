e41e507634cf55047a00421b01a5ff37b5a16478
Martin B <m.bogdanov9110@gmail.com>
2022-07-13 16:21:32 +0300

    Update mealService.js

--- client/src/services/mealService.js
+++ client/src/services/mealService.js
@@ -1,8 +1,27 @@
+import { endpoints } from "../API/endpoints";
 
-export const getOne = async (endpoint) => {
+export const getOne = async ({ userId }) => {
+    console.log(userId);
+    try {
+        const res = await fetch(endpoints.API_DETAILS(userId), {
+            mode: 'cors',
+            credentials: "include",
+            headers: {
+                'Content-Type': 'application/json',
+                'Allow-Control-Access-Policy': true,
+                "Access-Control-Allow-Credentials": true,
+            },
+        })
+        return await res.json();
+    } catch (error) {
+        throw new Error(error.message)
+    }
+};
+
+export const getAll = async () => {
 
     try {
-        const res = await fetch(endpoint, {
+        const res = await fetch(endpoints.API_BROWSE, {
             mode: 'cors',
             credentials: "include",
             headers: {
@@ -17,10 +36,10 @@ export const getOne = async (endpoint) => {
     }
 };
 
-export const getAll = async (endpoint) => {
+export const getOwn = async () => {
 
     try {
-        const res = await fetch(endpoint, {
+        const res = await fetch(endpoints.API_MYRECIPES, {
             mode: 'cors',
             credentials: "include",
             headers: {
@@ -35,20 +54,23 @@ export const getAll = async (endpoint) => {
     }
 };
 
-export const getOwn = async (endpoint) => {
+export const create = async (createData) => {
 
     try {
-        const res = await fetch(endpoint, {
+        const creteResult = await fetch(endpoints.API_ADD, {
+            method: "POST",
             mode: 'cors',
             credentials: "include",
             headers: {
                 'Content-Type': 'application/json',
                 'Allow-Control-Access-Policy': true,
                 "Access-Control-Allow-Credentials": true,
-            }
+            },
+            body: JSON.stringify(createData),
         });
-        return await res.json();
+        return await creteResult.json();
+
     } catch (error) {
         throw new Error(error.message)
     }
-};
+}
