222f9a9c96a5bfa771296dce7620d9b9dd81a9dc
Martin B <m.bogdanov9110@gmail.com>
2022-07-13 17:59:50 +0300

    added errorHander and error message to appear

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -1,25 +1,38 @@
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { getSession, setSession } from "../../API/api";
 import { userRegister } from "../../services/userService";
 
-export const Register = ({ setUser, setErrorMessage }) => {
+export const Register = ({ setUser, setErrorMessage, errorMessage, setIsLoading }) => {
 
     let navigate = useNavigate();
+
+    const [value, setValues] = useState({
+        email: '',
+        password: '',
+        rePassword: ''
+    });
+    const errorHandler = () => setErrorMessage('');
+    
+    const changeHandler = (e) => {
+        setValues(state => ({
+            ...state, [e.target.name]: e.target.value
+        }));
+    }
 
     const registerHandler = (e) => {
         e.preventDefault();
 
-        const formData = new FormData(e.target);
-        const { email, password, rePassword } = Object.fromEntries(formData);
-
-        userRegister({ email, password, rePassword })
+        userRegister(value)
             .then(res => {
                 console.log(res);
                 if (res.token) {
                     setSession(res.email, res.token, res.id);
+                    setUser(previous => getSession());
                     navigate('/recipe/browse');
-                    return setUser(previous => getSession());
+                    setIsLoading(false);
                 } else {
+                    console.log(res.message);
                     setErrorMessage({ error: res.message });
                     throw new Error(res.message);
                 }
@@ -28,16 +41,17 @@ export const Register = ({ setUser, setErrorMessage }) => {
 
     return (
         <div className="register-form">
+            <h3 style={{ color: "red" }}> {errorMessage && errorMessage.error}</h3>
             <h3 className="already-reg">Регистрация</h3>
             <form method="POST" onSubmit={registerHandler}>
                 <label className="already-reg" htmlFor="email">e-mail</label>
-                <input type="text" className="email" name="email" placeholder="e-mail..." required />
+                <input type="text" className="email" name="email" placeholder="e-mail..." required onChange={(changeHandler, errorHandler)} />
 
                 <label className="already-reg" htmlFor="password">парола</label>
-                <input type="password" className="password" name="password" placeholder="парола..." required />
+                <input type="password" className="password" name="password" placeholder="парола..." required onChange={(changeHandler, errorHandler)} />
 
                 <label className="already-reg" htmlFor="rePassword">повторете паролата</label>
-                <input type="password" className="rePassword" name="rePassword" placeholder="повторете паролата..." required />
+                <input type="password" className="rePassword" name="rePassword" placeholder="повторете паролата..." required onChange={(changeHandler, errorHandler)} />
 
                 <input className="already-reg" type="submit" value="Регистриране" />
             </form>
