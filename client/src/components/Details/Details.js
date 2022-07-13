a44ed62e578d518b7c87becc59af00a87f9cf216
Martin B <m.bogdanov9110@gmail.com>
2022-07-14 02:22:46 +0300

    update

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -4,7 +4,7 @@ import { getOne } from "../../services/mealService";
 import { Link, useParams } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
 
-export const Details = ({ isLoading, setIsLoading, setErrorMessage }) => {
+export const Details = ({ isLoading, setIsLoading, setErrorMessage, errorMessage }) => {
 
     const [meal, setMeal] = useState({});
     let id = useParams();
@@ -20,34 +20,38 @@ export const Details = ({ isLoading, setIsLoading, setErrorMessage }) => {
                     setErrorMessage({ error: res.message });
                     throw new Error(res.message);
                 }
-            });
+            })
     }, []);
 
-    console.log(meal);
-
     return (
-        <div className="details">
-            {
-                isLoading
-                    ? <BeatLoader loading={isLoading} />
-                    : <>
-                        <h1 className="meal-name">{meal.name}</h1>
-                        <a href={meal.image} target="_blank" rel="noreferrer"><img className="meal-details" src={meal.image}
-                            alt="" /></a>
-                        <div className="meal-buttons">
-                            {meal.owner !== undefined && meal.owner !== null
-                                ? <OnwerButtons meal={meal} />
-                                : <Link className="btn" to="/recipe/browse">Назад</Link>
-                            }
-                        </div>
-                        <article className="recipe-details">
-                            <label htmlFor="ingredients">Необходими съставки:</label>
-                            <p className="recipe" name="ingredients"><span>{meal.ingredients}</span></p>
-                            <label htmlFor="ingredients">Рецепта:</label>
-                            <p className="recipe" name="ingredients"><span>{meal.fullRecipe}</span></p>
-                        </article>
-                    </>
-            }
-        </div >
+        <>
+            <div className="details">
+                {
+                    isLoading
+                        ? <><BeatLoader loading={() => isLoading} /></>
+                        : <>
+                            <h1 className="meal-name">{meal.name}</h1>
+                            <a href={meal.image} target="_blank" rel="noreferrer"><img className="meal-details" src={meal.image}
+                                alt="" /></a>
+                            <div className="meal-buttons">
+                                {meal.owner !== undefined && meal.owner !== null
+                                    ? <OnwerButtons meal={meal} />
+                                    : <Link className="btn" to="/recipe/browse">Назад</Link>
+                                }
+                            </div>
+                            <article className="recipe-details">
+                                <label htmlFor="ingredients">Необходими съставки:</label>
+                                <p className="recipe" name="ingredients"><span>{meal.ingredients}</span></p>
+                                <label htmlFor="ingredients">Рецепта:</label>
+                                <p className="recipe" name="ingredients"><span>{meal.fullRecipe}</span></p>
+                            </article>
+                        </>
+                }
+                {errorMessage !== ""
+                    ? <p className="error-message"> {errorMessage.error}</p>
+                    : ""
+                }
+            </div >
+        </>
     );
 }
