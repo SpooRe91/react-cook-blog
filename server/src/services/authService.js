dcdd4bde4e2cfbe8ef5c345780df41d6e4aaa57e
Martin B <m.bogdanov9110@gmail.com>
2022-07-13 18:11:24 +0300

    amended da register function to have try{}catch{}

--- server/src/services/authService.js
+++ server/src/services/authService.js
@@ -4,23 +4,28 @@ const jwt = require('jsonwebtoken');
 
 const { SALT_ROUNDS } = require('../config/constants');
 const { secret } = require('../config/env');
+const { getErrorMessage } = require('../utils/errorHelpers');
 
 /*----------------------register--------------------------*/
 exports.register = async ({ email, password }) => {
-    const existing = await User.findOne({ email: new RegExp(`^${email}$`, 'i') });
+    try {
+        const existing = await User.findOne({ email: new RegExp(`^${email}$`, 'i') });
 
-    if (existing) {
-        throw new Error('Please enter another e-mail!');
-    };
+        if (existing) {
+            throw new Error('Please enter another e-mail!');
+        };
 
-    const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
+        const hashedPassword = await bcrypt.hash(password, SALT_ROUNDS);
 
-    let createdUser = await User.create({
-        email,
-        password: hashedPassword,
-    });
+        let createdUser = await User.create({
+            email,
+            password: hashedPassword,
+        });
 
-    return createdUser;
+        return createdUser;
+    } catch (error) {
+        return (getErrorMessage(error))
+    }
 };
 
 /*----------------------login--------------------------*/
