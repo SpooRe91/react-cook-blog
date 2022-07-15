06d0c77c806cd452ec8e0ec8b6c47c9cdb8ff2c9
Martin B <m.bogdanov9110@gmail.com>
2022-07-15 18:35:36 +0300

    Update MealContainer.js

--- client/src/components/MyRecipes/MealContainer.js
+++ client/src/components/MyRecipes/MealContainer.js
@@ -1,16 +1,80 @@
 import { Link } from "react-router-dom";
+import { FaHeart } from 'react-icons/fa'
+import { useEffect, useState } from "react";
+import { addLike } from "../../services/mealService";
 
-export const MealContainer = (props) => {
+export const MealContainer = ({
+    _id, name,
+    image, owner,
+    timesLiked, user,
+    setErrorMessage, errorMessage }) => {
+
+
+    const [numberOfLikes, setNumberOfLikes] = useState(0);
+
+    useEffect(() => {
+        if (timesLiked !== null && timesLiked !== undefined) {
+            setNumberOfLikes(timesLiked.length);
+
+        }
+    }, []);
+
+    const likeHandler = async (e) => {
+
+        if (!timesLiked.find(x => x === user.id)) {
+            try {
+                await addLike(_id);
+                e.target.style.display = "none";
+                setNumberOfLikes(likes => likes + 1);
+
+            } catch (error) {
+                setErrorMessage({ error: error.message })
+            }
+        } else {
+            setErrorMessage({ error: "You have already liked this recipe!" })
+        }
+    }
 
     return (
         <>
-            <div className="meal">
-                <Link to={`/details/${props._id}`} >
-                    <p className="name">{props.name}</p>
+            <div className="meal" data={_id}>
+                <Link to={`/details/${_id}`} >
+                    <p className="name">{name}</p>
                 </Link>
-                <Link to={`/details/${props._id}`} className="meal-image-link">
-                    <img className="meal" src={props.image} alt="" /></Link>
-                <Link className="btn" to={`/details/${props._id}`}>Подробно</Link>
+                <Link to={`/details/${_id}`} className="meal-image-link">
+                    <img className="meal" src={image} alt="" /></Link>
+                <Link className="meal-buttons" to={`/details/${_id}`}>Подробно</Link>
+                {
+                    user.id === owner
+                        ? <span><FaHeart className="like-icon" /> Total likes: {numberOfLikes} </span>
+                        :
+                        timesLiked !== null && timesLiked !== undefined
+                            ?
+                            timesLiked.find(x => x === user.id)
+                                ?
+                                <>
+                                    <span>You already liked this!</span>
+                                    <span><FaHeart className="like-icon" /> Харесано {numberOfLikes} пъти</span>
+                                </>
+                                :
+                                <>
+                                    <input type="button" className="name" onClick={(e) => likeHandler(e)} value="Like" />
+                                    {errorMessage
+                                        ? <p className="error-message"> {errorMessage.error}</p>
+                                        : ""
+                                    }
+                                    <span><FaHeart className="like-icon" /> Харесано {numberOfLikes} пъти</span>
+                                </>
+                            :
+                            <>
+                                <button className="name" onClick={(e) => likeHandler(e)}>Like</button>
+                                {errorMessage
+                                    ? <p className="error-message"> {errorMessage.error}</p>
+                                    : ""
+                                }
+                                <h4 className="meal">Няма храесвания</h4>
+                            </>
+                }
             </div >
         </>
     );
