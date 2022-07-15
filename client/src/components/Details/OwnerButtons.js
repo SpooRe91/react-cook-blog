897d5cd37a0aeb48b3c86d2dc074502e8baeafd6
Martin B <m.bogdanov9110@gmail.com>
2022-07-16 00:16:30 +0300

    update

--- client/src/components/Details/OwnerButtons.js
+++ client/src/components/Details/OwnerButtons.js
@@ -1,10 +1,18 @@
 import { Link } from "react-router-dom"
 export const OnwerButtons = (props) => {
+
     return (
         <>
             <Link className="btn-details" to={`/edit/${props.meal._id}`}>Промени</Link>
             <Link className="btn-details" to={`/delete/${props.meal._id}`}>Изтрии</Link>
-            <Link className="btn-details" to="/recipe/browse">Назад</Link>
+            <div className="dropdown">
+                <button className="dropbtn">&#11167; oбратно към &#11167;</button>
+                <div className="dropdown-content">
+                    <Link className="btn-details-back" to="/recipe/browse">към всички рецепти</Link>
+                    <Link className="btn-details-back" to="/recipe/browse">към моите рецепти</Link>
+                </div>
+            </div>
+
         </>
     );
 }
