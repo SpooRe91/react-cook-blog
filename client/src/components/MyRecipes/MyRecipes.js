c6b6dfb57572ad1b3b6c8a0203bc12e0d8b4ffa1
Martin B <m.bogdanov9110@gmail.com>
2022-07-15 18:37:06 +0300

    re-factored MyRecipes, so the search can dynamically work on input, also added conditional rendering, depending on the delete status,

--- client/src/components/MyRecipes/MyRecipes.js
+++ client/src/components/MyRecipes/MyRecipes.js
@@ -1,43 +1,76 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
-import { endpoints } from "../../API/endpoints";
 import { getOwn } from "../../services/mealService";
 import { MealContainer } from "./MealContainer"
 
-export const MyRecipes = ({ isLoading, setIsLoading, setErrorMessage }) => {
+export const MyRecipes = ({
+    user, isLoading,
+    setIsLoading, setErrorMessage,
+    errorMessage }) => {
 
     const [meals, setMeals] = useState([]);
 
     useEffect(() => {
-        getOwn(endpoints.API_MYRECIPES)
-            .then((res) => setMeals(res))
-            .then(() => setIsLoading(false))
-            .catch((err) => setErrorMessage(err));
-    }, [isLoading, setIsLoading, setErrorMessage]);
+        getOwn()
+            .then(res => {
+                if (res.length > 0) {
+                    setMeals(res);
+                    setIsLoading(false)
+                }
+            }).catch(error => {
+                console.log(error.message);
+                setErrorMessage({ error: error.message });
+            })
 
+    }, [setIsLoading, setErrorMessage]);
+
+    const [filterValue, setFilterValue] = useState("");
+
+    const filterHandler = (e) => {
+        setFilterValue(e.target.value.toLowerCase());
+    };
+
+    const notDeleted = meals.filter(x => x.isDeleted !== true);
+    console.log(notDeleted);
 
     return (
         <div className="search-container">
             <div>
                 <h1 className="already-reg">Моите рецепти</h1>
                 <form className="search" method="GET">
-                    <input type="text" placeholder="Търси..." name="search" />
-                    <input type="submit" value={'Търси'} />
+                    <input type="text" placeholder="Търси..." name="search"
+                        value={filterValue} onChange={filterHandler} />
                 </form>
             </div>
             {
-                isLoading
-                    ? <BeatLoader loading={isLoading} />
-                    : <div className="meal-containter">
-                        {meals.length > 0
-                            ? meals.map(meal => <MealContainer key={meal._id} {...meal} />)
-                            : <div className="already-reg">
-                                <p>За сега няма намерени рецепти, добавете рецепта <Link to="/recipe/add" className="already-reg">ТУК</Link></p>
-                            </div>
-                        }
-                    </div>
+                < div className="meal-containter">
+                    {
+                        isLoading
+                            ? <BeatLoader loading={isLoading} />
+                            : filterValue
+                                ? notDeleted.filter(x => x.name.toLowerCase().includes(filterValue))
+                                    .map(meal =>
+                                        <MealContainer key={meal._id} {...meal}
+                                            timesLiked={meal.likes} user={user}
+                                            setErrorMessage={setErrorMessage} errorMessage={errorMessage} />)
+                                :
+                                notDeleted !== undefined && notDeleted !== null && notDeleted.length > 0
+                                    ? notDeleted.map(meal =>
+                                        <MealContainer key={meal._id} {...meal}
+                                            timesLiked={meal.likes} user={user}
+                                            setErrorMessage={setErrorMessage} errorMessage={errorMessage} />)
+
+                                    : <div className="already-reg">
+                                        <p>За сега няма намерени рецепти, добавете рецепта <Link to="/recipe/add" className="already-reg">ТУК</Link></p>
+                                    </div>
+                    }
+                </div>
             }
-        </div>
+            {errorMessage
+                ? <p className="error-message"> {errorMessage.error}</p>
+                : ""
+            }
+        </div >
     );
 }
