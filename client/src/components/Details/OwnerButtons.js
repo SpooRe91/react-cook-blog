3a62e9a546fbf97502360ffab86fd0ad72a6d67c
Martin B <m.bogdanov9110@gmail.com>
2022-07-16 00:34:47 +0300

    update

--- client/src/components/Details/OwnerButtons.js
+++ client/src/components/Details/OwnerButtons.js
@@ -5,14 +5,6 @@ export const OnwerButtons = (props) => {
         <>
             <Link className="btn-details" to={`/edit/${props.meal._id}`}>Промени</Link>
             <Link className="btn-details" to={`/delete/${props.meal._id}`}>Изтрии</Link>
-            <div className="dropdown">
-                <button className="dropbtn">&#11167; oбратно към &#11167;</button>
-                <div className="dropdown-content">
-                    <Link className="btn-details-back" to="/recipe/browse">към всички рецепти</Link>
-                    <Link className="btn-details-back" to="/recipe/browse">към моите рецепти</Link>
-                </div>
-            </div>
-
         </>
     );
 }
