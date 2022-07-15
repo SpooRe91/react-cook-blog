1cc9f3bc4e8b81a42bc32d10f21193c6a6747df5
Martin B <m.bogdanov9110@gmail.com>
2022-07-15 03:40:35 +0300

    update

--- client/src/components/ErrorPage/ErrorPage.js
+++ client/src/components/ErrorPage/ErrorPage.js
@@ -1,12 +1,13 @@
-export const ErrorPage = ({errorMessage}) => {
+export const ErrorPage = ({ errorMessage }) => {
     console.log(errorMessage);
 
     return (
         <div className="container">
             <main>
+                <h1 className="auth-error">404</h1>
                 <>
                     {errorMessage
-                        ? <p style={{ "color": 'red' }}> {errorMessage.error}</p>
+                        ? <p className="error-message"> {errorMessage.error}</p>
                         : ""
                     }
                 </>
