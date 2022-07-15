8ec16a08707c6e1787dcc70505c348dcba2931a5
Martin B <m.bogdanov9110@gmail.com>
2022-07-15 18:33:01 +0300

    Update Browse.js

--- client/src/components/Browse/Browse.js
+++ client/src/components/Browse/Browse.js
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import { getAll } from "../../services/mealService";
 import { MealContainer } from "../MyRecipes/MealContainer";
 import { BeatLoader } from "react-spinners";
-export const Browse = ({ isLoading, setIsLoading, setErrorMessage, errorMessage }) => {
+export const Browse = ({ user, isLoading, setIsLoading, setErrorMessage, errorMessage }) => {
 
     const [meals, setMeals] = useState([]);
 
     useEffect(() => {
         getAll()
             .then(res => {
+                console.log(res);
                 if (res.length > 0) {
                     setMeals(res);
                     setIsLoading(false);
@@ -19,31 +20,54 @@ export const Browse = ({ isLoading, setIsLoading, setErrorMessage, errorMessage 
             });
     }, [setIsLoading, setErrorMessage]);
 
+    const [filterValue, setFilterValue] = useState("");
+
+    const filterHandler = (e) => {
+        setFilterValue(e.target.value.toLowerCase());
+    };
+
+    const notDeleted = meals.filter(x => x.isDeleted !== true);
+    console.log(notDeleted);
+
     return (
+
         <div className="search-container">
             <div>
-                {errorMessage
-                    ? <p className="error-message"> {errorMessage.error}</p>
-                    : ""
-                }
                 <h1 className="already-reg">Търсене на рецепти</h1>
                 <form className="search" method="GET">
-                    <input type="text" placeholder="Търси..." name="search" />
-                    <input type="submit" value={"Търси"} />
+                    <input type="text" placeholder="Търси..." name="search"
+                        value={filterValue} onChange={filterHandler} />
                 </form>
             </div>
             {
-                isLoading
-                    ? <BeatLoader loading={isLoading} />
-                    : <div className="meal-containter">
-                        {meals !== undefined && meals !== null && meals.length > 0
-                            ? meals.map(meal => <MealContainer key={meal._id} {...meal} />)
-                            : <div className="already-reg">
-                                <p>Все още няма рецепти!</p>
-                            </div>
-                        }
-                    </div>
+                < div className="meal-containter">
+                    {
+                        isLoading
+                            ? <BeatLoader loading={isLoading} />
+                            : filterValue
+                                ? notDeleted.filter(x => x.name.toLowerCase().includes(filterValue))
+                                    .map(meal =>
+                                        <MealContainer key={meal._id} {...meal}
+                                            user={user} timesLiked={meal.likes}
+                                            setErrorMessage={setErrorMessage} errorMessage={errorMessage} />)
+                                :
+                                notDeleted !== undefined && notDeleted !== null && notDeleted.length > 0
+                                    ? notDeleted.map(meal =>
+                                        <MealContainer key={meal._id} {...meal}
+                                            user={user} timesLiked={meal.likes}
+                                            setErrorMessage={setErrorMessage} errorMessage={errorMessage} />)
+
+                                    : <div className="already-reg">
+                                        <p>Все още няма рецепти!</p>
+                                    </div>
+                    }
+                </div>
             }
-        </div>
+            {errorMessage
+                ? <p className="error-message"> {errorMessage.error}</p>
+                : ""
+            }
+        </div >
+
     );
 }
