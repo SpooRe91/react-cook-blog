9ed63ecb054a4ad44a16a6f73706ee9f22f526c2
Martin B <m.bogdanov9110@gmail.com>
2022-07-16 18:28:59 +0300

    fixed a title in the nav

--- client/src/components/NavBars/GuestNavBar.js
+++ client/src/components/NavBars/GuestNavBar.js
@@ -10,7 +10,7 @@ export const GuestNavBar = ({ focusHandler }) => {
                 <NavLink to="/recipe/browse" className={focusHandler}>търсене на рецепти</NavLink>
             </li>
             <li>
-                <NavLink to="/recipe/macros" className={focusHandler}>хранителност</NavLink>
+                <NavLink to="/recipe/macros" className={focusHandler}>хранителни стойности</NavLink>
             </li>
             <li>
                 <NavLink to="/auth/register" className={focusHandler}>регистрация</NavLink>
