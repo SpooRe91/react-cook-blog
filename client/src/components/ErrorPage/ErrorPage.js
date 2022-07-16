23551869e21a1d76fbd85a62d0731db010556c6a
Martin B <m.bogdanov9110@gmail.com>
2022-07-16 18:26:33 +0300

    added Title and a styling

--- client/src/components/ErrorPage/ErrorPage.js
+++ client/src/components/ErrorPage/ErrorPage.js
@@ -1,16 +1,20 @@
+import { Link } from "react-router-dom";
+
 export const ErrorPage = ({ errorMessage }) => {
     console.log(errorMessage);
 
     return (
-        <div className="container">
-            <main>
-                <h1 className="auth-error">404</h1>
+        <div className="container" style={{ 'display': "flex" }}>
+            <title>Грешка...</title>
+            <main >
+                <h1 className="auth-error">{errorMessage}</h1>
                 <>
                     {errorMessage
                         ? <p className="error-message"> {errorMessage.error}</p>
                         : ""
                     }
                 </>
+                <Link to="/">Обратно към началната страница</Link>
             </main>
         </div>
     )
