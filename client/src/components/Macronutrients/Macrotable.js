635a09a4be06b4de0520c61a77d8dd18e50bb23a
Martin B <m.bogdanov9110@gmail.com>
2022-07-16 18:28:35 +0300

    crated Macrotable component

---
+++ client/src/components/Macronutrients/Macrotable.js
@@ -0,0 +1,23 @@
+export const Macrotable = ({ name, water, protein, fat, carb, calories, value }) => {
+
+    if (value === 0) {
+        value = 1
+    }
+
+    return (
+        <>
+            {
+                <tr>
+                    <td className='row-name' name={name}>{name}</td>
+                    <td className="row-values">{(water * value).toFixed(2)}</td>
+                    <td className="row-values">{(protein * value).toFixed(2)}</td>
+                    <td className="row-values">{(fat * value).toFixed(2)}</td>
+                    <td className="row-values">{(carb * value).toFixed(2)}</td>
+                    <td className='row-values'>{(calories * value).toFixed(2)}</td>
+                </tr>
+
+            }
+        </>
+    )
+
+}
