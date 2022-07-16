e63a7cc6ea22db06a4453842b3e563a07348dad5
Martin B <m.bogdanov9110@gmail.com>
2022-07-16 18:24:57 +0300

    added the localStorage as well

--- client/src/API/api.js
+++ client/src/API/api.js
@@ -2,13 +2,18 @@ export const setSession = (email, token, id) => {
 
     const user = { email, id, token };
     sessionStorage.setItem("User", JSON.stringify(user));
+    localStorage.setItem("User", JSON.stringify(user));
 }
 
 export const getSession = () => {
-    return JSON.parse(sessionStorage.getItem('User'))
+    return (
+        JSON.parse(sessionStorage.getItem('User')),
+        JSON.parse(localStorage.getItem('User'))
+    )
 }
 
 export const logoutSession = () => {
     sessionStorage.removeItem('User');
+    localStorage.removeItem('User');
 }
 
