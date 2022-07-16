85eca3463aed1fec8411be49d390551b728a52e0
Martin B <m.bogdanov9110@gmail.com>
2022-07-16 18:28:25 +0300

    added Macrotable to the render and added another logic to calculate on input

--- client/src/components/Macronutrients/Macronutrients.js
+++ client/src/components/Macronutrients/Macronutrients.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { BeatLoader } from 'react-spinners';
 import { getMacros } from '../../services/mealService';
+import { Macrotable } from './Macrotable';
+
 import styles from './Macronutrients.module.css';
 
 export const Macronutrients = ({ isLoading,
@@ -28,69 +30,70 @@ export const Macronutrients = ({ isLoading,
         }
     }, [setErrorMessage, setProducts, products, setIsLoading]);
 
-    const [filterValue, setFilterValue] = useState("")
+    const [filterValue, setFilterValue] = useState("");
+    const [quantify, setQuantify] = useState("");
 
     const filterHandler = (e) => {
         setFilterValue(e.target.value.toLowerCase());
     };
 
+    const quantityHandler = (e) => {
+        let value = Number(e.target.value);
+        setQuantify(value);
+        console.log(e.target.value);
+    };
 
     return (
         <>
+        <title>Хранителни стойности</title>
             <div className={styles['table-container']}>
                 {
                     isLoading
                         ? <><BeatLoader loading={() => isLoading} /></>
                         : <>
                             < h1 className="already-reg">Търсене на продукти</h1>
-                            < h3 className="already-reg">Моля въведете име на продукт:</h3>
-                            <form className="search" method="GET">
-                                <input type="text" placeholder="Търси..." name="search"
+                            < h3 className="already-reg">Моля въведете име на продукт на Български език:</h3>
+                            <form className="search-nutrients" method="GET">
+                                <label htmlFor='search'>Моля въведете име на Български</label>
+                                <input type="text" placeholder="пилешко..." name="search"
                                     value={filterValue} onChange={filterHandler} />
-                                <input type="submit" value={"Търси"} />
+                                <label htmlFor='quantity'>Моля въведете име количество в грамове</label>
+                                <input type="number" placeholder="1000гр..." name="quantity"
+                                    value={quantify || ''} onChange={quantityHandler} />
                             </form>
                             <h3 className="already-reg">На тази таблица можете да намерите основните хранителни
-                                стойности на най-често срещаните и употребявани храни и продукти!</h3>
+                                стойности на най-често срещаните и употребявани продукти!</h3>
+                            <h3 className="already-reg">Стойностите са в грамове и се отнасят за 100гр. продукт!</h3>
                             <div className="row">
                                 <div className="col-xs-12">
+                                    <caption className="already-reg">!!!Важно:
+                                        Данните относителни,и може да същестуват разминавания с други източници!
+                                    </caption>
                                     <table>
                                         <thead>
                                             <tr>
                                                 <th>Име на продукт</th>
-                                                <th>Вода</th>
-                                                <th>Белтъчини</th>
-                                                <th>Мазнини</th>
-                                                <th>Въглехидрати</th>
-                                                <th>Калории</th>
+                                                <th>Вода гр.</th>
+                                                <th>Белтъчини гр.</th>
+                                                <th>Мазнини гр.</th>
+                                                <th>Въглехидрати гр.</th>
+                                                <th>Калории гр.</th>
                                             </tr>
                                         </thead>
                                         <tbody>
 
                                             {/* филтриране по време на писане, ако има нещо въведено в state 
-                                            тогава работим с първия филтър, ако ли не изобразяваме всичко */}
-
-                                            {filterValue
-                                                ? products.filter(p => p.name.toLowerCase().includes(filterValue))
-                                                    .map(p => <tr key={p._id}>
-                                                        <td className='row-name' name={p.name}>{p.name}</td>
-                                                        <td className="row-values">{p.water}</td>
-                                                        <td className="row-values">{p.protein}</td>
-                                                        <td className="row-values">{p.fat}</td>
-                                                        <td className="row-values">{p.carb}</td>
-                                                        <td className='row-values'>{p.calories}</td>
-                                                    </tr>)
-                                                : products.map(x =>
-                                                    <tr key={x._id}>
-                                                        <td className='row-name' name={x.name}>{x.name}</td>
-                                                        <td className="row-values">{x.water}</td>
-                                                        <td className="row-values">{x.protein}</td>
-                                                        <td className="row-values">{x.carb}</td>
-                                                        <td className="row-values">{x.fat}</td>
-                                                        <td className='row-values'>{x.calories}</td>
-                                                    </tr>
-                                                )
-                                            }
-
+                                            тогава работим с първия филтър, ако ли не изобразяваме всичко,
+                                            също при въвеждане на стойност самостоятелно или с име, се променят и стойностите */}
+                                            <>
+                                                {
+                                                    quantify !== 0 || filterValue
+                                                        ?
+                                                        products.filter(p => p.name.toLowerCase().includes(filterValue))
+                                                            .map(row => <Macrotable key={row._id} {...row} value={quantify / 100} />)
+                                                        : products.map(row => <Macrotable key={row._id} {...row} value={1} />)
+                                                }
+                                            </>
                                         </tbody>
                                     </table>
                                 </div>
