9d81c803661b576be4a334d9e94cdf9a14fa4329
Martin B <m.bogdanov9110@gmail.com>
2022-07-16 18:26:41 +0300

    added Title

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,14 +1,16 @@
 export const Homepage = () => {
 
     return (
-        <article className="landing-page-article">
-            <div>
-                <section className="landing-page-section">
-                    <h1 className="landing-page-header"><i>Welcome to Cook-Blog</i></h1>
-                    <h3 className="landing-page-header"><i>Your favourite meal awaits you!</i></h3>
-                </section>
-            </div>
-        </article>
+        <>
+            <title>Начало</title>
+            <article className="landing-page-article">
+                <div>
+                    <section className="landing-page-section">
+                        <h1 className="landing-page-header"><i>Welcome to Cook-Blog</i></h1>
+                        <h3 className="landing-page-header"><i>Your favourite meal awaits you!</i></h3>
+                    </section>
+                </div>
+            </article>
+        </>
     );
-
 }
