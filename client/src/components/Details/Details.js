56bcbd040d2bdde2aff332b97b62156f2de97917
Martin B <m.bogdanov9110@gmail.com>
2022-07-17 23:28:25 +0300

    added the likes option to the details screen as well

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
 import { OnwerButtons } from "./OwnerButtons"
-import { getOne } from "../../services/mealService";
-import { Link, useParams } from "react-router-dom";
+import { addLike, getOne } from "../../services/mealService";
+import { useParams } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
 import { FaHeart } from 'react-icons/fa'
 
 export const Details = ({ user, isLoading, setIsLoading, setErrorMessage, errorMessage }) => {
 
     const [meal, setMeal] = useState({});
-    const [numberOfLikes, setNumberOfLikes] = useState(0);
     const { mealId } = useParams();
+
+    const [numberOfLikes, setNumberOfLikes] = useState(0);
+    const [arrayOfLikes, setArrayOfLikes] = useState([]);
 
     useEffect(() => {
         getOne(mealId)
             .then(res => {
                 if (res._id) {
                     setMeal(res);
+                    setArrayOfLikes(res.likes);
                     setNumberOfLikes(res.likes.length);
                     setIsLoading(false);
                 } else {
@@ -26,9 +29,27 @@ export const Details = ({ user, isLoading, setIsLoading, setErrorMessage, errorM
             })
     }, [mealId, setIsLoading, setErrorMessage]);
 
+    console.log(arrayOfLikes);
+
+    const likeHandler = async (e) => {
+
+        if (!arrayOfLikes.find(x => x === user.id)) {
+            try {
+                await addLike(meal._id);
+                e.target.style.display = "none";
+                setNumberOfLikes(likes => likes + 1);
+
+            } catch (error) {
+                setErrorMessage({ error: error.message })
+            }
+        } else {
+            setErrorMessage({ error: "You have already liked this recipe!" })
+        }
+    }
+
     return (
         <>
-        <title>Детайли: {meal.name}</title>
+            <title>Детайли: {meal.name}</title>
             <div className="details">
                 {
                     isLoading
@@ -37,12 +58,47 @@ export const Details = ({ user, isLoading, setIsLoading, setErrorMessage, errorM
                             <h1 className="meal-name">{meal.name}</h1>
                             <a href={meal.image} target={"_blank"} rel="noreferrer"><img className="meal-details" src={meal.image}
                                 alt="" /></a>
-                            <div >
+                            <div>
                                 {
-                                    user && meal.owner === user.id && <OnwerButtons meal={meal} />
+                                    user && meal.owner === user.id && <OnwerButtons {...meal} />
                                 }
                             </div>
-                            <p className="recipe" name="name"><span>Брой харесвания: {numberOfLikes} <FaHeart /> </span></p>
+
+                            {
+                                arrayOfLikes !== null && arrayOfLikes !== undefined
+                                    ? //if we have likes on the current item
+                                    user
+                                        ? //if we have logged user
+                                        user.id === meal.owner
+                                            ? //if the logged user is owner
+                                            <span className="number-of-likes"><FaHeart className="like-icon" /> Общо харесвания: {numberOfLikes} </span>
+                                            ://if the logged user is not owner
+                                            arrayOfLikes.find(x => x === user.id)
+                                                ? //if the logged user liked this already
+                                                <>
+                                                    <span className="number-of-likes">Харесано от Вас!</span>
+                                                    <span className="number-of-likes"><FaHeart className="like-icon" />
+                                                        Харесано {numberOfLikes > 1 ? `${numberOfLikes} пъти` : `${numberOfLikes} път`}</span>
+                                                </>
+                                                ://if the logged user has not liked it yet
+                                                <>
+                                                    <input type="button" className="name" onClick={(e) => likeHandler(e)} value="Like" />
+                                                    {errorMessage
+                                                        ? <p className="error-message"> {errorMessage.error}</p>
+                                                        : ""
+                                                    }
+                                                    <span className="number-of-likes"><FaHeart className="like-icon" />
+                                                        Харесано {numberOfLikes > 1 ? `${numberOfLikes} пъти` : `${numberOfLikes} път`}</span>
+                                                </>
+                                        ://if there is no logged user
+                                        <span className="number-of-likes"><FaHeart className="like-icon" /> Общо харесвания: {numberOfLikes} </span>
+                                    ://if there are no likes
+                                    <>
+                                        <button className="name" onClick={(e) => likeHandler(e)}>Like</button>
+                                        <h4 className="meal">Няма храесвания</h4>
+                                    </>
+                            }
+                            {/* <p className="recipe" name="name"><span>Брой харесвания: {numberOfLikes} <FaHeart /> </span></p> */}
                             <article className="recipe-details">
                                 <label htmlFor="ingredients">Необходими съставки:</label>
                                 <p className="recipe" name="ingredients"><span>{meal.ingredients}</span></p>
