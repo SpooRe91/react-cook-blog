412837c5be1789e80849349c06527fba42008f8a
Martin B <m.bogdanov9110@gmail.com>
2022-07-18 22:35:24 +0300

    translated into bg

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -8,13 +8,13 @@ export const About = ({ setIsOpen }) => {
                 <div className="already-reg">
                     <h1 className="already-reg">Относно <span>"Cook-blog by Martin Bogdanov"</span></h1>
                     <p className="about-text">
-                        "Cook-blog" is a basic SPA, created with React-app.
-                        It allows for CRUD operations by making AJAX requests to a custom REST Api. It was written by myself using with Express.js.
-                        The back-end is running on Node.js.
+                        "Cook-Blog" e SP приложение, създадено с React JS.
+                        Има способността да извършва CRUD операции чрез AJAX заявки към REST API написан от мен.
+                        REST API e писан с Express.js и върви на Node.js
                     </p>
                     <p className="about-text">
-                        Please enjoy the SPA and don't hesitate to <strong>contact</strong> me for further
-                        suggestions at <span> <strong>m.bogdanov9110@gmail.com</strong></span>
+                        Надявам се, да ви хареса това малко приложение, ако имате препоръки или забележки, моля <strong>свържете се</strong> с мен на
+                        <span> <strong>m.bogdanov9110@gmail.com</strong></span>
                     </p>
                     <div>
                         {/*button which closes the modal, when clicked */}
