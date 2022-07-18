6e5c7eddb0407822f8cf023160e52ea46d49e74c
Martin B <m.bogdanov9110@gmail.com>
2022-07-18 19:30:54 +0300

    removed all conditions, as the recipes are only the user's

--- client/src/components/MyRecipes/MealContainer.js
+++ client/src/components/MyRecipes/MealContainer.js
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 import { FaHeart } from 'react-icons/fa'
 import { useEffect, useState } from "react";
-import { addLike } from "../../services/mealService";
 
 export const MealContainer = ({
-    _id, name,
-    image, owner,
-    timesLiked, user,
-    setErrorMessage, errorMessage }) => {
+    _id,
+    name,
+    image,
+    timesLiked,
+    errorMessage }) => {
 
 
     const [numberOfLikes, setNumberOfLikes] = useState(0);
@@ -18,22 +18,6 @@ export const MealContainer = ({
 
         }
     }, [timesLiked]);
-
-    const likeHandler = async (e) => {
-
-        if (!timesLiked.find(x => x === user.id)) {
-            try {
-                await addLike(_id);
-                e.target.style.display = "none";
-                setNumberOfLikes(likes => likes + 1);
-
-            } catch (error) {
-                setErrorMessage({ error: error.message })
-            }
-        } else {
-            setErrorMessage({ error: "Вече сте харесали тази рецепта!" })
-        }
-    }
 
     return (
         <>
@@ -47,43 +31,16 @@ export const MealContainer = ({
                 {
                     numberOfLikes !== 0
                         ? //if we have likes on the current item
-                        user
-                            ? //if we have logged user
-                            user.id === owner
-                                ? //if the logged user is owner
-                                <span className="number-of-likes"><FaHeart className="like-icon" /> Общо харесвания: {numberOfLikes} </span>
-                                ://if the logged user is not owner
-                                timesLiked.find(x => x === user.id)
-                                    ? //if the logged user liked this already
-                                    <>
-                                        <span>Харесано от Вас!</span>
-                                        <span className="number-of-likes"><FaHeart className="like-icon" /> Харесано {numberOfLikes > 1 ? `${numberOfLikes} пъти` : `${numberOfLikes} път`}</span>
-                                    </>
-                                    ://if the logged user has not liked it yet
-                                    <>
-                                        <input type="button" className="name" onClick={(e) => likeHandler(e)} value="Харесай" />
-                                        {errorMessage
-                                            ? <p className="error-message"> {errorMessage.error}</p>
-                                            : ""
-                                        }
-                                        <span className="number-of-likes"> <FaHeart className="like-icon" /> Харесано {numberOfLikes > 1 ? `${numberOfLikes} пъти` : `${numberOfLikes} път`}</span>
-                                    </>
-                            ://if there is no logged user
-                            <span className="number-of-likes"><FaHeart className="like-icon" /> Общо харесвания: {numberOfLikes} </span>
-                        ://if there are no likes
-                        <>
-                            {/* if there are no likes, and the user is not the owner */}
-                            {user && user.id !== owner &&
-                                <input type="button" className="name" onClick={(e) => likeHandler(e)} value="Харесай" />
-                            }
-
-                            {errorMessage
-                                ? <p className="error-message"> {errorMessage.error}</p>
-                                : ""
-                            }
-
+                        <span className="number-of-likes">
+                            <FaHeart className="like-icon" /> {numberOfLikes === 1 ? `${numberOfLikes} храесване` : `${numberOfLikes} харесвания`} </span>
+                        : <>
+                            {/* //if there is no logged user */}
                             <span className="meal">Няма храесвания</span>
                         </>
+                }
+                {errorMessage
+                    ? <p className="error-message"> {errorMessage.error}</p>
+                    : ""
                 }
             </div >
         </>
