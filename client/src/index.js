2de57db75e84c8cacdf5ba0ec3d058987f341764
Martin B <m.bogdanov9110@gmail.com>
2022-07-18 22:35:00 +0300

    imported global css

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import "./css/site.css"
 import { CookiesProvider } from 'react-cookie';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
