38815978fbb8c1aa2aa55c2cd9c10f2801609580
Martin B <m.bogdanov9110@gmail.com>
2022-07-20 21:40:29 +0300

    update

--- client/src/components/Browse/Browse.js
+++ client/src/components/Browse/Browse.js
@@ -1,6 +1,9 @@
 import { useEffect, useState } from "react";
-import { getAll } from "../../services/mealService";
 import { MealContainer } from "./MealContainer";
+import { ScrollButton } from "./ScrollButton";
+
+
+import { getAll } from "../../services/mealService";
 import { BeatLoader } from "react-spinners";
 export const Browse = ({ user, isLoading, setIsLoading, setErrorMessage, errorMessage }) => {
 
@@ -38,63 +41,64 @@ export const Browse = ({ user, isLoading, setIsLoading, setErrorMessage, errorMe
     };
 
     return (
-
-        <div className="search-container">
-            <title>Търсене на рецепти</title>
-            <div>
-                <h1 className="already-reg">Търсене на рецепти</h1>
-                <form className="search" method="GET">
-                    {<input type="text" className="recipe-browse" placeholder="Търси..." name="search"
-                        value={filterValue} onChange={filterHandler} />}
-                </form>
-            </div>
-            {
-                <>
-                    < input type="button" defaultValue={toLoad ? "Покажи скорошни" : "Покажи всички"} onClick={toLoadHandler} />
-                    <p className="arrow">Резултатите от търсенето се отразяват на изобразяването на рецептите, ако няма резултат, нищо няма да се изобрази.</p>
-                    <h1 className="already-reg">{toLoad ?
-                        <p className="arrow">&#11167; Всички рецепти (scroll-нете надолу) &#11167;</p>
-                        : <p className="arrow">Най-скорокорошни рецепти</p>}</h1>
-
-                </>
-            }
-            {
-                < div className="meal-containter">
-                    {
-                        isLoading
-                            ? <BeatLoader loading={isLoading} />
-                            : filterValue
-                                ? notDeleted.filter(x => x.name.toLowerCase().includes(filterValue))
-                                    .map(meal =>
-                                        <MealContainer key={meal._id} {...meal}
-                                            user={user} timesLiked={meal.likes}
-                                            setErrorMessage={setErrorMessage} errorMessage={errorMessage} />)
-                                :
-                                notDeleted !== undefined && notDeleted !== null && notDeleted.length > 0
-                                    ?
-                                    recipesToShow.map(meal =>
-                                        <MealContainer key={meal._id} {...meal}
-                                            user={user} timesLiked={meal.likes}
-                                            setErrorMessage={setErrorMessage} errorMessage={errorMessage} />)
-                                    : <div className="already-reg">
-                                        <p>Все още няма рецепти!</p>
-                                    </div>
-                    }
-                    {
-                        (toLoad && !filterValue)
-                        &&
-                        moreRecipesToLoad.map(meal => <MealContainer key={meal._id} {...meal}
-                            user={user} timesLiked={meal.likes}
-                            setErrorMessage={setErrorMessage} errorMessage={errorMessage} />)
-                    }
+        <>
+            <div className="search-container">
+                <title>Търсене на рецепти</title>
+                <div>
+                    <h1 className="already-reg">Търсене на рецепти</h1>
+                    <form className="search" method="GET">
+                        {<input type="text" className="recipe-browse" placeholder="Търси..." name="search"
+                            value={filterValue} onChange={filterHandler} />}
+                    </form>
                 </div>
+                {
+                    <>
+                        < input type="button" defaultValue={toLoad ? "Покажи скорошни" : "Покажи всички"} onClick={toLoadHandler} />
+                        <p className="arrow">Резултатите от търсенето се отразяват на изобразяването на рецептите, ако няма резултат, нищо няма да се изобрази.</p>
+                        <h1 className="already-reg">{toLoad ?
+                            <p className="arrow">&#11167; Всички рецепти (scroll-нете надолу) &#11167;</p>
+                            : <p className="arrow">Най-скорокорошни рецепти</p>}</h1>
+                    </>
+                }
+                {
+                    < div className="meal-containter">
+                        {
+                            isLoading
+                                ?
+                                <BeatLoader loading={isLoading} />
+                                :
+                                filterValue
+                                    ?
+                                    notDeleted.filter(x => x.name.toLowerCase().includes(filterValue))
+                                        .map(meal =>
+                                            <MealContainer key={meal._id} {...meal}
+                                                user={user} timesLiked={meal.likes}
+                                                setErrorMessage={setErrorMessage} errorMessage={errorMessage} />)
+                                    :
+                                    notDeleted !== undefined && notDeleted !== null && notDeleted.length > 0
+                                        ?
+                                        recipesToShow.map(meal =>
+                                            <MealContainer key={meal._id} {...meal}
+                                                user={user} timesLiked={meal.likes}
+                                                setErrorMessage={setErrorMessage} errorMessage={errorMessage} />)
+                                        :
+                                        <div className="already-reg">
+                                            <p>Все още няма рецепти!</p>
+                                        </div>
+                        }
+                        {
+                            (toLoad && !filterValue)
+                            &&
+                            moreRecipesToLoad.map(meal => <MealContainer key={meal._id} {...meal}
+                                user={user} timesLiked={meal.likes}
+                                setErrorMessage={setErrorMessage} errorMessage={errorMessage} />)
+                        }
+                    </div>
+                }
+                {errorMessage && <p className="error-message"> {errorMessage.error}</p>}
 
-            }
-            {
-                errorMessage &&
-                <p className="error-message"> {errorMessage.error}</p>
-            }
-        </div >
-
+            </div>
+            {<ScrollButton />}
+        </>
     );
 }
