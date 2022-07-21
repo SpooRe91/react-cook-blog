0807c744bcc9ae03486046e83f456a11e9015bc7
Martin B <m.bogdanov9110@gmail.com>
2022-07-22 02:39:10 +0300

    added description, and some styling to the addRecipe

--- client/src/components/AddRecipe/AddRecipe.js
+++ client/src/components/AddRecipe/AddRecipe.js
@@ -46,32 +46,56 @@ export const AddRecipe = ({ errorMessage, setErrorMessage, setIsLoading }) => {
     return (
 
         <div>
-            <title>Добави рецепта</title>
-            {errorMessage !== ""
-                ? <p className="error-message"> {errorMessage.error}</p>
-                : ""
+            {errorMessage !== "" &&
+                <p className="error-message"> {errorMessage.error}</p>
             }
+            <title>Добави рецепта</title>
+
             <h1 className="already-reg">Добави рецепта</h1>
 
             <form className="add-form" method="POST" onSubmit={createHandler}>
                 <div className="already-reg">
-                    <label htmlFor="name">Име</label>
-                    <input type="text" name="name" onChange={changeHandler} required value={values.name} />
+                    <label htmlFor="name">име</label>
+                    <input type="text" name="name" id="name" onChange={changeHandler}
+                        placeholder="някакво име..." required value={values.name} />
                 </div>
                 <div className="already-reg">
-                    <label htmlFor="image">Снимка</label>
-                    <input type="text" name="image" onChange={changeHandler} required value={values.image} />
+                    <label htmlFor="image">снимка</label>
+                    <input type="text" name="image" id="image" onChange={changeHandler}
+                        placeholder={"https://some-image.com..."} required value={values.image} />
                 </div>
                 <div className="already-reg">
-                    <label htmlFor="fullRecipe">Пълна рецепта</label>
-                    <textarea className="add-recipe-text" type="text" name="fullRecipe" onChange={changeHandler} value={values.fullRecipe} required />
+                    <label htmlFor="fullRecipe">пълна рецепта</label>
+                    <textarea className="add-recipe-text" type="text" id="fullRecipe" name="fullRecipe"
+                        onChange={changeHandler} value={values.fullRecipe} placeholder="Първата стъпка е..." required />
                 </div >
                 <div className="already-reg">
-                    <label htmlFor="ingredients">Необходими продукти</label>
-                    <textarea className="add-recipe-text" type=" text" name="ingredients" onChange={changeHandler} value={values.ingredients} required />
+                    <label htmlFor="ingredients">необходими продукти</label>
+                    <textarea className="add-recipe-text" type=" text" id="ingredients" name="ingredients"
+                        onChange={changeHandler} value={values.ingredients} placeholder="Продукт 1, продукт 2..." required />
                 </div>
-                <input type="submit" value="Създай" />
+                <input type="submit" value="създай" className="add-form-submit" />
             </form >
+            <div>
+                <article className="recipe-details">
+                    <h1 className="already-reg">Кратка информация:</h1>
+                    <p className="recipe-add">
+                        1. Моля въведете името на рецептата на Български език, в полето "име".
+                    </p>
+                    <p className="recipe-add">
+                        2. Моля въведете валиден URL започващ с http:// или https://
+                        или можете да качите снимка на вашето ястие, като натиснете бутона "качи симка".
+                    </p>
+                    <p className="recipe-add">
+                        3. В полето "пълна рецепта", моля въведете пълното описание на рецептата. Имайте предвид,
+                        че форматирането на текста се запазва.
+                    </p>
+                    <p className="recipe-add">
+                        4. Докато изброявате продуктите в полето "необходими продукти", имайте предвид,
+                        че форматирането на текста се запазва, така че, ако желаете можете да ги въвеждате един под друг.
+                    </p>
+                </article>
+            </div>
         </div >
     );
 }
