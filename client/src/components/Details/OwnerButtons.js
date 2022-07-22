2367b86709ab1ad94127fb91d81e07b2b5a9f720
Martin B <m.bogdanov9110@gmail.com>
2022-07-22 16:33:36 +0300

    OwnerButtons.js creted delete handler and state to confirm the delete

--- client/src/components/Details/OwnerButtons.js
+++ client/src/components/Details/OwnerButtons.js
@@ -1,12 +1,47 @@
-import { Link } from "react-router-dom"
-export const OnwerButtons = ({ _id }) => {
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom"
+import { deleteMeal } from "../../services/mealService";
+export const OnwerButtons = ({ _id, setErrorMessage }) => {
+
+    const navigate = useNavigate();
+
+    const [isClicked, setIsClicked] = useState(false);
+
+    const deleteHandler = (e) => {
+        deleteMeal(_id)
+            .then(res => {
+                if (res.status === 202) {
+
+                    navigate('/recipe/myRecipes');
+                }
+                console.log(res);
+                if (res.message) throw new Error(res.message);
+            })
+            .catch(error => {
+                setErrorMessage({ error: error.message })
+            });
+    };
+
+    const confirmHandler = (e) => {
+        setIsClicked(state => !state);
+    }
 
     return (
         <>
-            <div className="owner-buttons">
-                <Link className="btn-details" to={`/edit/${_id}`}>Промени</Link>
-                <Link className="btn-details" to={`/delete/${_id}`}>Изтрии</Link>
-            </div>
+            {
+                isClicked
+                    ?
+                    <div className="owner-buttons">
+                        <p>Сигурни ли сте, че искате да изтриете рецептата?</p>
+                        <input type="button" className="delete-confirm-btn" onClick={deleteHandler} value="да" />
+                        <input type="button" className="delete-confirm-btn" onClick={confirmHandler} value="не"/>
+                    </div>
+                    :
+                    <div className="owner-buttons">
+                        <Link className="btn-details" to={`/edit/${_id}`}>промени</Link>
+                        <input type="button" className="delete-confirm-btn" onClick={confirmHandler} value="изтрий" />
+                    </div>
+            }
         </>
     );
 }
