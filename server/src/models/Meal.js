c40fd16538e187ff6136d42b5af00618b27f315e
Martin B <m.bogdanov9110@gmail.com>
2022-07-23 18:55:55 +0300

    updates and added portions and difficulty to the Meal model - back-end

--- server/src/models/Meal.js
+++ server/src/models/Meal.js
@@ -4,25 +4,33 @@ const URL_PATERN = /^https?:\/\/(.+)/;
 const mealSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: [true, 'Recipe name is requred!'],
+        required: [true, 'Име на рецептата е задължително!'],
     },
     image: {
         type: String,
-        required: [true, 'Imgage URL is required!'],
+        required: [true, 'URL на снимакта е задължителен!'],
         validate: {
             validator: function (value) {
                 return URL_PATERN.test(value);
             },
-            message: "Image must be a valid URL"
+            message: "URL, на снимката трябва да е валиден!"
         }
     },
     fullRecipe: {
-        required: [true, 'Full recipe is requred!'],
+        required: [true, 'Опсианиен а рецептата е задължително!'],
         type: String,
     },
     ingredients: {
         type: String,
-        required: [true, 'Incredients are requred!']
+        required: [true, 'Необходимите съставки са задължителни!'],
+    },
+    portions: {
+        type: Number,
+        required: [true, 'Моля въведете брой порции!'],
+    },
+    difficulty: {
+        type: String,
+        required: [true, 'Моля въведете трудност за изпълнение на рецептата!'],
     },
     owner: {
         type: mongoose.Types.ObjectId,
