a6ea9e53004da49f6817f0668b3e2ddb50f6c1af
Martin B <m.bogdanov9110@gmail.com>
2022-07-24 18:22:07 +0300

    updated LOGIN error, and added pseudo route guard

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,10 +3,12 @@ import { Link, useNavigate } from "react-router-dom";
 import { getSession, setSession } from "../../API/api";
 import { userLogin } from "../../services/userService";
 
+import { LoggedUserContext } from "../../contexts/LoggedUserContext";
 import { ErrorContext } from "../../contexts/ErrorMessageContext";
 
 export const Login = ({ setUser, setIsLoading }) => {
 
+    const user = useContext(LoggedUserContext);
     const { errorMessage, setErrorMessage } = useContext(ErrorContext);
     const navigate = useNavigate();
 
@@ -56,24 +58,40 @@ export const Login = ({ setUser, setIsLoading }) => {
                     <div className="error-container">
                         <p className="error-message">
                             {errorMessage.error}
-                            <button className="btn" onClick={() => setErrorMessage('')}>OK</button>
+                            <button className="btn" onClick={() => setErrorMessage('')}>
+                                OK
+                            </button>
                         </p>
                     </div>
                 }
-                <h3 className="already-reg">Вход</h3>
-                <form className="login-form" method="POST" onSubmit={loginHandler}>
-                    <label className="credentials" htmlFor="email">e-mail</label>
-                    <input type="text" className="email" id="email" name="email"
-                        placeholder="e-mail..." required onChange={changeHandler} value={value.email} />
+                <>
+                    {
+                        user ?
+                            <div className="error-container">
+                                <p className="error-message">
+                                    {"Вече сте влязли!"}
+                                    <button className="btn" onClick={() => [setErrorMessage(''), navigate('/')]}>OK</button>
+                                </p>
+                            </div>
+                            :
+                            <>
+                                <h3 className="already-reg">Вход</h3>
+                                <form className="login-form" method="POST" onSubmit={loginHandler}>
+                                    <label className="credentials" htmlFor="email">e-mail</label>
+                                    <input type="text" className="email" id="email" name="email"
+                                        placeholder="e-mail..." required onChange={changeHandler} value={value.email} />
 
-                    <label className="credentials" htmlFor="password">парола</label>
-                    <input type="password" className="password" id="password" name="password"
-                        placeholder="парола..." required onChange={changeHandler} value={value.password} />
+                                    <label className="credentials" htmlFor="password">парола</label>
+                                    <input type="password" className="password" id="password" name="password"
+                                        placeholder="парола..." required onChange={changeHandler} value={value.password} />
 
-                    <input className="already-reg" type="submit" value="Вход" />
-                </form>
+                                    <input className="already-reg" type="submit" value="Вход" />
+                                </form>
 
-                <h3 className="already-reg">Нямате регистрация? <Link to="/auth/register">Регистрирайте се тук!</Link></h3>
+                                <h3 className="already-reg">Нямате регистрация? <Link to="/auth/register">Регистрирайте се тук!</Link></h3>
+                            </>
+                    }
+                </>
             </div >
         </>
     );
