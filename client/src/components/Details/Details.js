69a796da269689d3e0713030a46049b55c23c2e2
Martin B <m.bogdanov9110@gmail.com>
2022-07-24 18:21:34 +0300

    updated Details

--- client/src/components/Details/Details.js
+++ client/src/components/Details/Details.js
@@ -1,5 +1,5 @@
 import { useContext, useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
 import { FaHeart } from 'react-icons/fa'
 
@@ -12,34 +12,33 @@ import { ErrorContext } from "../../contexts/ErrorMessageContext";
 
 export const Details = ({ isLoading, setIsLoading }) => {
 
+    const navigate = useNavigate();
     const user = useContext(LoggedUserContext);
     const { errorMessage, setErrorMessage } = useContext(ErrorContext);
 
     const [meal, setMeal] = useState({});
     const { mealId } = useParams();
 
-    const [numberOfLikes, setNumberOfLikes] = useState(0);
-    const [arrayOfLikes, setArrayOfLikes] = useState([]);
+    const [numberOfLikes, setNumberOfLikes] = useState(null);
+    const [arrayOfLikes, setArrayOfLikes] = useState(null);
 
     const [isLiked, setIsLiked] = useState(false);
 
     useEffect(() => {
         getOne(mealId)
             .then(res => {
-                if (!res.isDeleted) {
+                if (!res.isDeleted && res._id) {
                     setMeal(res);
                     setArrayOfLikes(res.likes);
                     setNumberOfLikes(res.likes.length);
                     setIsLoading(false);
-                } else {
-                    throw new Error("Не е намерена такава рецепта!");
                 }
                 if (res.message) throw new Error(res.message);
                 setIsLoading(false);
             })
             .catch(error => {
                 console.log(error.message);
-                setErrorMessage({ error: error.message });
+                return setErrorMessage({ error: error.message });
             });
         return () => {
             setErrorMessage('');
@@ -67,6 +66,7 @@ export const Details = ({ isLoading, setIsLoading }) => {
         }
     }
 
+
     const likeHeartWithCount = (
         <span className="number-of-likes">
             <FaHeart className="number-of-likes" style={isLiked || user?.id === meal.owner
@@ -75,90 +75,108 @@ export const Details = ({ isLoading, setIsLoading }) => {
             />{numberOfLikes}
         </span>);
 
+
     const likeButton = (
         <button type="button" className="like-button"
             onClick={(e) => likeHandler(e)}>харесай &#11166;{likeHeartWithCount}
         </button>)
 
+
     let createdOn = new Date(meal.updatedAt).toString();
     createdOn = createdOn.slice(0, createdOn.indexOf('GMT'));
 
+
     return (
         <>
-            <title>Детайли: {meal.name}</title>
-            {errorMessage !== "" &&
-                <div className="error-container">
-                    <p className="error-message">
-                        {errorMessage.error}
-                        <button className="btn" onClick={() => setErrorMessage('')}>OK</button>
-                    </p>
-                </div>
-            }
-            <div className="details">
-                {
-                    isLoading
-                        ?
-                        <>
-                            <BeatLoader loading={() => isLoading} />
-                        </>
-                        :
-                        <>
-                            <h1 className="meal-name">
-                                {meal.name}
-                            </h1>
-                            <a href={meal.image} target={"_blank"} rel="noreferrer"><img className="meal-details" src={meal.image}
-                                alt="" />
-                            </a>
-                            <div>
-                                {user && meal.owner === user?.id && <OnwerButtons {...meal} setErrorMessage={setErrorMessage} />}
-                            </div>
-                            <div className="like-container">
-                                {
-                                    user !== null && user?.id !== meal.owner
-                                        ?//if we have a logged user and is not the owner
-                                        numberOfLikes !== 0
-                                            ?//if there are likes
-                                            isLiked
-                                                ?//if the current element is liked by the logged user
-                                                <span>Харесано от Вас! {likeHeartWithCount}</span>
-                                                ://if  it's not liked by the logged user
-                                                <>
-                                                    {likeButton}
-                                                </>
-                                            ://if there are no likes and the user can like it
-                                            <>
-                                                <span className="meal">Няма харесвания</span>
-                                                {likeButton}
-                                            </>
-                                        ://if there is no logged user
-                                        likeHeartWithCount
-                                }
-                            </div>
-
+            {
+                errorMessage !== ""
+                    ?
+                    <div className="error-container">
+                        <p className="error-message">
+                            {errorMessage.error}
+                            <button className="btn" onClick={() => [setErrorMessage(''), navigate('/')]}>OK</button>
+                        </p>
+                    </div>
+                    :
+                    <>
+                        <title>Детайли: {meal.name}</title>
+                        <div className="details">
                             {
-                                user && user?.id !== meal.owner &&
-                                <>
-                                    < p className="created-by-details"><span >Създадено: {createdOn}</span> </p>
-                                    < p className="created-by-details"><span >Създадено от: {meal.ownerName}</span> </p>
-                                </>
+                                isLoading
+                                    ?
+                                    <>
+                                        <BeatLoader loading={() => isLoading} />
+                                    </>
+                                    :
+                                    <>
+                                        <h1 className="meal-name">
+                                            {meal.name}
+                                        </h1>
+                                        <a href={meal.image} target={"_blank"} rel="noreferrer"><img className="meal-details" src={meal.image}
+                                            alt="" />
+                                        </a>
+                                        <div>
+                                            {user && meal?.owner === user?.id && <OnwerButtons {...meal} setErrorMessage={setErrorMessage} />}
+                                        </div>
+                                        <div className="like-container">
+                                            {
+                                                user !== null && user?.id !== meal.owner
+                                                    ?//if we have a logged user and is not the owner
+                                                    numberOfLikes !== 0
+                                                        ?//if there are likes
+                                                        isLiked
+                                                            ?//if the current element is liked by the logged user
+                                                            <span>Харесано от Вас! {likeHeartWithCount}</span>
+                                                            ://if  it's not liked by the logged user
+                                                            <>
+                                                                {likeButton}
+                                                            </>
+                                                        ://if there are no likes and the user can like it
+                                                        <>
+                                                            <span className="meal">Няма харесвания</span>
+                                                            {likeButton}
+                                                        </>
+                                                    ://if there is no logged user
+                                                    likeHeartWithCount
+                                            }
+                                        </div>
+                                    </>
                             }
-                        </>
-                }
-                <article>
-                    <p className="recipe-diff-count">порции: <strong>{meal.portions}</strong></p>
-                    <p className="recipe-diff-count">сложност: <strong>{meal.difficulty}</strong></p>
+                            <article>
 
-                </article>
-                {<ScrollButton />}
-            </div>
+                                <p className="recipe-diff-count" style={{ "color": "white" }}>порции:
+                                    <strong style={{ "color": "wheat" }}>{meal.portions}</strong>
+                                </p>
 
-            <article className="recipe-details">
-                <label htmlFor="ingredients">Необходими съставки:</label>
-                <p className="recipe" name="ingredients"><span>{meal.ingredients}</span></p>
+                                <p className="recipe-diff-count" style={{ "color": "white" }}>сложност:
+                                    <strong style={{ "color": "wheat" }}>{meal.difficulty}</strong>
+                                </p>
 
-                <label htmlFor="ingredients">Рецепта:</label>
-                <p className="recipe" name="ingredients"><span>{meal.fullRecipe}</span></p>
-            </article>
+                                < p className="created-by-details">Създадено:
+                                    <span style={meal.updatedAt ? { "color": "wheat" } : { color: "" }} >
+                                        {createdOn}
+                                    </span>
+                                </p>
+
+                                < p className="created-by-details">Създадено от:
+                                    <span style={meal.ownerName ? { "color": "wheat" } : { color: "" }}>
+                                        {meal.ownerName}
+                                    </span>
+                                </p>
+
+                            </article>
+                            {<ScrollButton />}
+                        </div>
+
+                        <article className="recipe-details">
+                            <label htmlFor="ingredients">Необходими съставки:</label>
+                            <p className="recipe" name="ingredients"><span>{meal.ingredients}</span></p>
+
+                            <label htmlFor="ingredients">Рецепта:</label>
+                            <p className="recipe" name="ingredients"><span>{meal.fullRecipe}</span></p>
+                        </article>
+                    </>
+            }
         </>
     );
 }
