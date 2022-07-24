f28ca98218501f71fbf939b446b385702f9b4426
Martin B <m.bogdanov9110@gmail.com>
2022-07-25 01:17:49 +0300

    removed Access-Control-Allow-Headers from the corsMiddleware - Access-Control-Allow-Headers header

--- server/src/middlewares/corsMiddleware.js
+++ server/src/middlewares/corsMiddleware.js
@@ -6,6 +6,6 @@ module.exports = () => (req, res, next) => {
     res.setHeader("Allow-Control-Access-Policy", "true");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
-    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Access-Control-Allow-Credentials, Allow-Control-Access-Policy, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Allow-Control-Access-Policy, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 };
