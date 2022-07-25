bfb7e8799b472d74a69aa35b62ca0f0869eda6d7
Martin B <m.bogdanov9110@gmail.com>
2022-07-25 18:28:33 +0300

    update

--- client/src/firebase/firebase-config.js
+++ client/src/firebase/firebase-config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getStorage, ref, deleteObject, } from "firebase/storage"
+import { getStorage} from "firebase/storage"
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
