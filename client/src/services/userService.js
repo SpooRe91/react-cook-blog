650ebaf08cb9a93bc175b83e0c5e9edeb496c82f
Martin B <m.bogdanov9110@gmail.com>
2022-07-25 18:25:29 +0300

    addded createProfile and editProfile functions

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -57,3 +57,41 @@ export const userLogout = async () => {
         throw new Error(error.message);
     }
 }
+
+export const createProfile = async (profileData) => {
+
+    try {
+        const profileResult = await fetch(endpoints.API_CREATE_PROFILE, {
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Allow-Control-Access-Policy': true,
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify(profileData),
+        });
+        return await profileResult.json();
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
+
+export const editProfile = async (editDate, id) => {
+
+    try {
+        const profileResult = await fetch(endpoints.API_EDIT_PROFILE(id), {
+            mode: 'cors',
+            credentials: 'include',
+            headers: {
+                'Content-Type': 'application/json',
+                'Allow-Control-Access-Policy': true,
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify(editDate),
+        });
+        return await profileResult.json();
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
