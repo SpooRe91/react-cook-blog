f03c711dfc27cd9b1cbe1dda9a24cc9734abe4ce
Martin B <m.bogdanov9110@gmail.com>
2022-07-25 18:28:11 +0300

    added createProfile, editProfile and Profile model to the foodService

--- server/src/services/foodService.js
+++ server/src/services/foodService.js
@@ -1,5 +1,6 @@
 const { Meal } = require('../models/Meal');
 const { Nutrient } = require('../models/Nutrient');
+const { Profile } = require('../models/Profile');
 
 exports.getAll = (search) => Meal.find({ name: { $regex: new RegExp(search, "i") } }).lean();
 
@@ -18,6 +19,10 @@ exports.delete = (id) => Meal.updateOne({ _id: id }, { $set: { isDeleted: true }
 exports.createMeal = (recipe) => Meal.create(recipe);
 
 exports.createNutrition = (data) => Nutrient.create(data);
+
+exports.createProfile = (data) => Profile.create(data);
+
+exports.editProfile = (data, id) => Profile.updateOne({ _id: id }, { $set: data }, { runValidators: true });
 
 exports.getOwner = (meal, user) => {
 
