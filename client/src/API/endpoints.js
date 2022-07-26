9fff437170aa9c5bba40a173e79e3ef263431c70
Martin B <m.bogdanov9110@gmail.com>
2022-07-27 01:41:26 +0300

    added getUser and EDIT user image endpoints

--- client/src/API/endpoints.js
+++ client/src/API/endpoints.js
@@ -7,8 +7,8 @@ export const endpoints = {
     API_EDIT: (id) => `${API_URL}/edit/${id}`,//PUT req
     API_DELETE: (id) => `${API_URL}/delete/${id}`,//DELETE req
     API_LIKE: (id) => `${API_URL}/like/${id}`,//GET req
-    API_CREATE_PROFILE: `${API_URL}/profile-create`,//POST req
-    API_EDIT_PROFILE: (id) => `${API_URL}/profile-edit/${id}`,//PUT req
+    API_GET_USER: (id) => `${API_URL}/user-get/${id}`,//POST req
+    API_EDIT_USER_IMAGE: (id) => `${API_URL}/user-edit/${id}`,//PUT req
     //AUTH**********************************
     API_REGISTER: `${API_URL}/auth/register`,//POST req
     API_LOGIN: `${API_URL}/auth/login`,//POST req
