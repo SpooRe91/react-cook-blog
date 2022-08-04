f1ac1329b12f6586a36b43802a8607496e274582
Martin B <m.bogdanov9110@gmail.com>
2022-08-04 18:13:34 +0300

    updates on macrotable

--- client/src/components/Macronutrients/Macrotable.js
+++ client/src/components/Macronutrients/Macrotable.js
@@ -1,17 +1,15 @@
-export const Macrotable = ({ name, water, protein, fat, carb, calories, value }) => {
-
-    if (value === 0) value = 1
+export const Macrotable = ({ name, water, protein, fat, carb, calories, value, setErrorMessage }) => {
 
     return (
         <>
             {
                 <tr>
-                    <td className='row-name' name={name}>{name}</td>
-                    <td className="row-values">{(water * value).toFixed(2)}</td>
-                    <td className="row-values">{(protein * value).toFixed(2)}</td>
-                    <td className="row-values">{(fat * value).toFixed(2)}</td>
-                    <td className="row-values">{(carb * value).toFixed(2)}</td>
-                    <td className='row-values'>{(calories * value).toFixed(2)}</td>
+                    <td name={name}>{name}</td>
+                    <td>{(water * value).toFixed(2)}</td>
+                    <td>{(protein * value).toFixed(2)}</td>
+                    <td>{(fat * value).toFixed(2)}</td>
+                    <td>{(carb * value).toFixed(2)}</td>
+                    <td>{(calories * value).toFixed(2)}</td>
                 </tr>
 
             }
