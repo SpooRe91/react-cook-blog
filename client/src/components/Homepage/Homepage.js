a85bf4579e2a692746ef915cf6d62c502ce33410
Martin B <m.bogdanov9110@gmail.com>
2022-08-04 18:12:57 +0300

    added Homepage css module

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,13 +1,15 @@
+import styles from "./Homepage.module.css";
+
 export const Homepage = () => {
 
     return (
         <>
             <title>Начало</title>
-            <article className="landing-page-article">
+            <article className={styles["landing-page-article"]}>
                 <div>
-                    <section className="landing-page-section">
-                        <h1 className="landing-page-header"><i>Welcome to Cook-Blog</i></h1>
-                        <h3 className="landing-page-header"><i>Your favourite meal awaits you!</i></h3>
+                    <section className={styles["landing-page-section"]}>
+                        <h1 className={styles["landing-page-header"]}><i>Welcome to Cook-Blog</i></h1>
+                        <h3 className={styles["landing-page-header"]}><i>Your favourite meal awaits YOU!</i></h3>
                     </section>
                 </div>
             </article>
