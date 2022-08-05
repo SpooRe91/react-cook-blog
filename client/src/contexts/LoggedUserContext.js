738db5d443393811a17c3069ce0f0beac1f3dd35
Martin B <m.bogdanov9110@gmail.com>
2022-08-05 13:52:45 +0300

    updated LoggedUserContext - added cookies, setting the user and

--- client/src/contexts/LoggedUserContext.js
+++ client/src/contexts/LoggedUserContext.js
@@ -1,3 +1,30 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import Cookies from "universal-cookie";
+import { getSession } from "../API/api";
 
 export const LoggedUserContext = createContext();
+
+const cookies = new Cookies();
+
+export const LoggedUserProvider = ({ children }) => {
+
+    const [user, setUser] = useState(getSession());
+    const [clientCookie, setClientCookie] = useState(cookies.get('user-session'));
+
+    const userHandler = (userInfo) => {
+        setUser(userInfo);
+    };
+
+    useEffect(() => {
+        if (getSession() !== null) {
+            cookies.set('user-session', getSession(), { path: "/", maxAge: 48000 });
+            setClientCookie(cookies.get('user-session'));
+        }
+    }, [setClientCookie])
+
+    return (
+        <LoggedUserContext.Provider value={{ user, userHandler, clientCookie, setClientCookie, cookies }}>
+            {children}
+        </LoggedUserContext.Provider>
+    )
+}
