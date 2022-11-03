5618a4a2b3faa5714985767b1f0169974227f32b
Martin B <m.bogdanov9110@gmail.com>
2022-11-03 19:57:07 +0200

    switched the cors allowed origin to localhost

--- server/src/middlewares/corsMiddleware.js
+++ server/src/middlewares/corsMiddleware.js
@@ -2,7 +2,7 @@ module.exports = () => (req, res, next) => {
 
     // 'http://localhost:3000' 'https://cook-blog-d3ed8.web.app'
 
-    res.setHeader('Access-Control-Allow-Origin', 'https://cook-blog-d3ed8.web.app');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.setHeader("Allow-Control-Access-Policy", "true");
     res.setHeader("Access-Control-Allow-Credentials", "true");
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS, HEAD');
