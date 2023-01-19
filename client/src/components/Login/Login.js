5c3283a04922a884576c4dbb2fe160a492487301
Martin B <m.bogdanov9110@gmail.com>
2023-01-19 17:10:01 +0200

    fixed the loader, added a style for it and re-factured the JSX

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -7,8 +7,9 @@ import { getSession, setSession } from "../../API/api";
 //-------------------------------------------------------------------------------------------------
 import { LoggedUserContext } from "../../contexts/LoggedUserContext";
 import { ErrorContext } from "../../contexts/ErrorMessageContext";
+import { BeatLoader } from "react-spinners";
 
-export const Login = ({ setIsLoading }) => {
+export const Login = ({ isLoading, setIsLoading }) => {
 
     //-------------------------------------------------------------------------------------------------
     const { userHandler, cookies } = useContext(LoggedUserContext);
@@ -29,7 +30,7 @@ export const Login = ({ setIsLoading }) => {
     //-------------------------------------------------------------------------------------------------
     const loginHandler = (e) => {
         e.preventDefault();
-
+        setIsLoading(state => true);
         userLogin(value)
             .then(res => {
                 if (res.token) {
@@ -37,7 +38,7 @@ export const Login = ({ setIsLoading }) => {
                     userHandler(getSession());
                     cookies.set('user-session', getSession(), { path: "/", maxAge: 48000 });
                     navigate('/recipe/browse', { replace: true });
-                    setIsLoading(false);
+                    setIsLoading(state => false);
                 }
                 if (res.message) throw new Error(res.message);
             })
@@ -51,30 +52,40 @@ export const Login = ({ setIsLoading }) => {
         return () => {
             setErrorMessage('');
             userHandler(getSession());
-            console.log(getSession());
         };
-    }, [setErrorMessage, userHandler]);
+    }, [setIsLoading, setErrorMessage, userHandler]);
     //-------------------------------------------------------------------------------------------------
 
     return (
         <>
             <title>Вход</title>
+            {isLoading ? "LOADING..." : null}
             <div>
                 <>
+                    {errorMessage !== '' && <p>Грешка</p>}
                     {errorMessage !== "" &&
                         <div className={styles["error-container"]}>
                             <p className={styles["error-message"]}>
                                 {errorMessage}
-                                <button className={styles["btn"]} onClick={() => setErrorMessage('')}>
+                                <button className={styles["btn"]} onClick={() => [setErrorMessage(''), setIsLoading(state => false)]}>
                                     OK
                                 </button>
                             </p>
                         </div>
                     }
-                    {
+                    {isLoading
+                        ?
                         <>
-                            <h3 className={styles["already-reg"]}>Вход</h3>
+                            <div className={styles["already-reg-loading"]}>
+                                <h3 className={styles["already-reg"]}>Вход</h3>
+                                <BeatLoader loading={() => isLoading} color={"white"} />
+                                <p>Моля изчакайте...</p>
+                            </div>
+                        </>
+                        :
+                        <>
                             <form className={styles["login-form"]} method="POST" onSubmit={loginHandler}>
+                                <h3 className={styles["already-reg"]}>Вход</h3>
                                 <label className={styles["credentials"]} htmlFor="email">e-mail</label>
                                 <input type="text" className={styles["email"]} id="email" name="email"
                                     placeholder="e-mail..." required onChange={changeHandler} value={value.email} />
@@ -84,8 +95,8 @@ export const Login = ({ setIsLoading }) => {
                                     placeholder="парола..." required onChange={changeHandler} value={value.password} />
 
                                 <input className={styles["already-reg"]} type="submit" value="Вход" />
+                                <h3 className={styles["already-reg"]}>Нямате регистрация? <Link to="/auth/register">Регистрирайте се тук!</Link></h3>
                             </form>
-                            <h3 className={styles["already-reg"]}>Нямате регистрация? <Link to="/auth/register">Регистрирайте се тук!</Link></h3>
                         </>
                     }
                 </>
