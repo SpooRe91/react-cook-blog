f8ed62aa6c56109e4af4eff0ae2501e6838be524
Martin B <m.bogdanov9110@gmail.com>
2023-04-05 18:11:37 +0300

    refactored and added more details to Homepage, also added paralx effect to the sections

--- client/src/components/Homepage/Homepage.js
+++ client/src/components/Homepage/Homepage.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./Homepage.module.css";
 
 export const Homepage = () => {
@@ -5,14 +6,52 @@ export const Homepage = () => {
     return (
         <>
             <title>Начало</title>
-            <article className={styles["landing-page-article"]}>
+            <section className={styles["landing-page-article"]}>
                 <div>
-                    <section className={styles["landing-page-section"]}>
-                        <h1 className={styles["landing-page-header"]}><i>Welcome to Cook-Blog</i></h1>
-                        <h3 className={styles["landing-page-header"]}><i>Your favourite meal awaits YOU!</i></h3>
-                    </section>
+                    <article className={styles["landing-page-section"]}>
+                        <h1 className={styles["landing-page-header"]}>Welcome to Cook-Blog</h1>
+                        <h3 className={styles["landing-page-header"]}>Your favourite meal awaits YOU!</h3>
+                    </article>
                 </div>
-            </article>
+            </section>
+            <div className={styles['second-header']}>
+                <section className={styles["landing-page-section1"]}>
+
+                    <article>
+                        <h4 className={styles["landing-page-header1"]}>
+                            Here you can browse different recipes and share your own
+                        </h4>
+                        <h4 className={styles["landing-page-header1"]}>
+                            Checkout the newest recipes over <Link to="recipe/browse">here</Link>
+                        </h4>
+                    </article>
+
+                </section>
+
+                <section className={styles["landing-page-section1"]}>
+                    <article>
+                        <h4 className={styles["landing-page-header1"]}>
+                            So hop on, and explore the culinary world
+                        </h4>
+                        <h4 className={styles["landing-page-header1"]}>
+                            You can register from <Link to="auth/register">here</Link> or
+                            login from <Link to="auth/login">here</Link>
+                        </h4>
+                    </article>
+                </section>
+
+                <section className={styles["landing-page-section1"]}>
+                    <article>
+                        <h4 className={styles["landing-page-header1"]}>
+                            All you need to do is register and start
+                        </h4>
+
+                        <h4 className={styles["landing-page-header1"]}>
+                            We also have a macronutrients table, right <Link to="auth/register">here</Link>
+                        </h4>
+                    </article>
+                </section>
+            </div>
         </>
     );
 }
