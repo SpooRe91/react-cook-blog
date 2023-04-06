fb3a1e806f4692b5f809785e4e160b6e874bd576
Martin B <m.bogdanov9110@gmail.com>
2023-04-06 17:55:08 +0300

    re-factored the adding of props to all components

--- client/src/App.js
+++ client/src/App.js
@@ -26,7 +26,7 @@ import { ErrorContextProvider } from "./contexts/ErrorMessageContext";
 function App() {
 
   const [isOpen, setIsOpen] = useState({ state: false, target: null });
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   return (
     <ErrorContextProvider>
@@ -35,13 +35,13 @@ function App() {
         <div className="App">
 
           {isOpen && isOpen.target === "logout" &&
-            <Logout isLoading={isLoading} setIsLoading={setIsLoading} setIsOpen={setIsOpen} />}
+            <Logout {...{ isLoading, setIsLoading, setIsOpen }} />}
 
           <Routes>
             <Route path="/" element={<Homepage />} />
             <Route element={<LoggedUserGuard />} >
-              <Route path="/auth/login" element={<Login isLoading={isLoading} setIsLoading={setIsLoading} />} />
-              <Route path="/auth/Register" element={<Register isLoading={isLoading} setIsLoading={setIsLoading} />} />
+              <Route path="/auth/login" element={<Login {...{ isLoading, setIsLoading }} />} />
+              <Route path="/auth/Register" element={<Register {...{ isLoading, setIsLoading }} />} />
             </Route>
 
             <Route path="/404"
@@ -49,26 +49,26 @@ function App() {
             />
             <Route element={<NoUserGuard />} >
               <Route path="/auth/profile/:id"
-                element={<Profile isLoading={isLoading} setIsLoading={setIsLoading} />} />
+                element={<Profile {...{ isLoading, setIsLoading }} />} />
               <Route path="/recipe/myRecipes"
-                element={<MyRecipes isLoading={isLoading} setIsLoading={setIsLoading} />}
+                element={<MyRecipes {...{ isLoading, setIsLoading }} />}
               />
               <Route path="/edit/:mealId"
-                element={<EditRecipe isLoading={isLoading} setIsLoading={setIsLoading} />}
+                element={<EditRecipe {...{ isLoading, setIsLoading }} />}
               />
               <Route path="/recipe/add"
-                element={<AddRecipe isLoading={isLoading} setIsLoading={setIsLoading} />}
+                element={<AddRecipe {...{ isLoading, setIsLoading }} />}
               />
             </Route>
 
             <Route path="/recipe/browse" element={
-              <Browse isLoading={isLoading} setIsLoading={setIsLoading} />}
+              <Browse {...{ isLoading, setIsLoading }} />}
             />
             <Route path="/details/:mealId" element={
-              <Details isLoading={isLoading} setIsLoading={setIsLoading} />}
+              <Details {...{ isLoading, setIsLoading }} />}
             />
             <Route path="/recipe/macros" element={
-              <Macronutrients isLoading={isLoading} setIsLoading={setIsLoading} />}
+              <Macronutrients {...{ isLoading, setIsLoading }} />}
             />
             <Route path="*" element={< ErrorPage />} />
 
