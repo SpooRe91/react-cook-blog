2d853b65a14e03ab6953daa9f9137705f68e776a
Martin B <m.bogdanov9110@gmail.com>
2023-04-09 13:08:03 +0300

    updated API main URL

--- client/src/API/endpoints.js
+++ client/src/API/endpoints.js
@@ -1,5 +1,5 @@
 
-export const API_URL = "https://react-cook-server.onrender.com";//GET
+export const API_URL = "https://react-cook-server.vercel.app";//GET
 
 export const endpoints = {
     //REGULAR + MODIFICATIONS***************
